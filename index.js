4d2d38e192c6e0b40a9cbf1c4a0dfadc4f80ac28
Julien Constant <julienconstant190@gmail.com>
2020-08-25 20:54:16 +0000

    revamp using prefix

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+prefix = process.env.PREFIX;
+token = process.env.CLIENT_TOKEN;
+
 client.on("ready", () => {
 	client.user.setActivity("https://faithful-dungeons.github.io/Website/", {type: "PLAYING"});
 	console.log("I am turned on lmao");
@@ -16,61 +19,54 @@ function attachIsImage(msgAttach) {
 client.on("message", message => {
   // Bot messages aren't read:
   if (message.author.bot) return;
+  
+  // COMMANDS WITH PREFIX
+  if (message.content.startsWith('${prefix}ping')) {
+    message.channel.send('Pong!');
+  }
+  if (message.content.startsWith('${prefix}help')) {
+    message.channel.send('JavaScript is ~~Awesome~~');
+  }
+  if (message.content.startsWith('${prefix}behave')) {
+    message.channel.reply("I'm so sorry! (⌯˃̶᷄ ﹏ ˂̶᷄⌯)")
+  }
 
-  // This is the usual argument parsing we love to use.
-  const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-
-  // COMMANDS WITH PREFIX:
-  // if there is the prefix at the begining of the message
-  if (message.content.indexOf(process.env.PREFIX) === 1){
-    if (command === 'ping') {
-      message.channel.send('Pong!');
-    }
-    if (command === 'help') {
-      message.channel.send('JavaScript is ~~Awesome~~');
-    }
-    if (command === 'behave') {
-      message.channel.reply("I'm so sorry! (⌯˃̶᷄ ﹏ ˂̶᷄⌯)")
-    }
-  } 
   // COMMANDS WITHOUT PREFIX:
-  else {
-    // Submit texture feature:
-    // id: 747889024068485180 -> #submit-texture (Robert's testing discord)
-    if (message.channel.id === '747889024068485180') {
-      // if message have a file attached:
-      if (message.attachments.size > 0) {
-        // run function to test url to see if file is an img
+  // Submit texture feature:
+  // id: 747889024068485180 -> #submit-texture (Robert's testing discord)
+  if (message.channel.id === '747889024068485180') {
+    // if message have a file attached:
+    if (message.attachments.size > 0) {
+      // run function to test url to see if file is an img
         if (message.attachments.every(attachIsImage)){
 
-          // If message doesn't have the texture path:
-          if(!message.content.includes('(')) {
-            message.reply("You need to add the texture path to your texture submission, following this example: texture (file1/file2/texture.png)").then(msg => {
-              msg.delete({timeout: 5000});
-            });
-          } else try {
-            message.react('✅').then(() => {message.react('❌')});
-          } catch (error) {
-            console.error("ERROR | One of the emojis failed to react!");
-          }
-
-        } else {
-          message.reply("Your texture submission needs to have an image file!").then(msg => {
+        // If message doesn't have the texture path:
+        if(!message.content.includes('(')) {
+          message.reply("You need to add the texture path to your texture submission, following this example: texture (file1/file2/texture.png)").then(msg => {
             msg.delete({timeout: 5000});
           });
+        } else try {
+          message.react('✅').then(() => {message.react('❌')});
+        } catch (error) {
+          console.error("ERROR | One of the emojis failed to react!");
         }
+
       } else {
-        message.reply("You need to attach a png file!").then(msg => {
+        message.reply("Your texture submission needs to have an image file!").then(msg => {
           msg.delete({timeout: 5000});
         });
       }
+    } else {
+      message.reply("You need to attach a png file!").then(msg => {
+        msg.delete({timeout: 5000});
+      });
     }
   }
 
+  // Check texture feature
   // All channel without #submit-texture
   if (message.channel.id !== '747889024068485180' ) {
-    if(message.content.includes('#4393')) {
+    if(message.content.startsWith('${prefix}#4393')) {
       const exampleEmbed = new Discord.MessageEmbed()
         .setColor('#dd7735')
         .setTitle('dirt_highblockhalls.png')
@@ -86,4 +82,4 @@ client.on("message", message => {
   }
 });
 
-client.login(process.env.CLIENT_TOKEN);
+client.login(token);
