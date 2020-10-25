fef3237fde4001426d431ce63c5dfcc7210baff0
Robert Reichel <robertreich1212@gmail.com>
2020-10-25 12:22:43 +0000

    start working on settings

--- commands/ping.js
+++ commands/ping.js
@@ -1,71 +1,72 @@
-const Discord = require('discord.js');
+const Discord  = require('discord.js');
+const settings = require('../settings.js');
 
 module.exports = {
 	name: 'ping',
 	description: 'Pong!',
 	execute(message, args) {
     //Faithful Dungeons
-    if (message.guild.id === '714910830272970834') {
+    if (message.guild.id === settings.FDungeonsID) {
 		  message.channel.send('Pinging...').then(m => {
         var ping = m.createdTimestamp - message.createdTimestamp;
 
         var embed = new Discord.MessageEmbed()
           .setTitle('Your ping is:')
           .setDescription('**' + ping + 'ms**')
-          .setColor('#E1631F')
-          .setFooter('Faithful Dungeons', 'https://i.imgur.com/ldI5hDM.png');
+          .setColor(settings.FDungeonsColor)
+          .setFooter('Faithful Dungeons', settings.FDungeonsIMG);
 
         m.edit(embed);
       });
       //Faithful Mods
-    } else if (message.guild.id === '748264625962877019') { 
+    } else if (message.guild.id === settings.FModsID) { 
 		  message.channel.send('Pinging...').then(m => {
         var ping = m.createdTimestamp - message.createdTimestamp;
 
         var embed = new Discord.MessageEmbed()
           .setTitle('Your ping is:')
           .setDescription('**' + ping + 'ms**')
-          .setColor('#915E00')
-          .setFooter('Faithful Mods', 'https://i.imgur.com/scpOogK.png');
+          .setColor(settings.FModsColor)
+          .setFooter('Faithful Mods', settings.FModsIMG);
 
         m.edit(embed);
       });
       //Faithful Tweaks
-    } else if (message.guild.id === '720966967325884426') { 
+    } else if (message.guild.id === settings.FTweaksID) { 
 		  message.channel.send('Pinging...').then(m => {
         var ping = m.createdTimestamp - message.createdTimestamp;
 
         var embed = new Discord.MessageEmbed()
           .setTitle('Your ping is:')
           .setDescription('**' + ping + 'ms**')
-          .setColor('#C4905A')
-          .setFooter('Faithful Tweaks', 'https://i.imgur.com/4U1ZSYT.png');
+          .setColor(settings.FTweaksColor)
+          .setFooter('Faithful Tweaks', settings.FTweaksIMG);
 
         m.edit(embed);
       });
       //Faithful Addons
-    } else if (message.guild.id === '614160586032414845') { 
+    } else if (message.guild.id === settings.FAddonsID) { 
 		  message.channel.send('Pinging...').then(m => {
         var ping = m.createdTimestamp - message.createdTimestamp;
 
         var embed = new Discord.MessageEmbed()
           .setTitle('Your ping is:')
           .setDescription('**' + ping + 'ms**')
-          .setColor('#47E9D6')
-          .setFooter('Faithful Addons', 'https://i.imgur.com/OSpMYU6.png');
+          .setColor(settings.FAddonsColor)
+          .setFooter('Faithful Addons', settings.FAddonsIMG);
 
         m.edit(embed);
       });
       //Faithful Traditional
-    } else if (message.guild.id === '766856721712545824') { 
+    } else if (message.guild.id === settings.FTraditionalID) { 
 		  message.channel.send('Pinging...').then(m => {
         var ping = m.createdTimestamp - message.createdTimestamp;
 
         var embed = new Discord.MessageEmbed()
           .setTitle('Your ping is:')
           .setDescription('**' + ping + 'ms**')
-          .setColor('#FFD800')
-          .setFooter('Faithful Traditional', 'https://i.imgur.com/SLpji2x.png');
+          .setColor(settings.FTraditionalColor)
+          .setFooter('Faithful Traditional', settings.FTraditionalIMG);
 
         m.edit(embed);
       });
@@ -77,8 +78,8 @@ module.exports = {
         var embed = new Discord.MessageEmbed()
           .setTitle('Your ping is:')
           .setDescription('**' + ping + 'ms**')
-          .setColor('#FFBB00')
-          .setFooter('Faithful Team', 'https://i.imgur.com/W1pfHe0.png');
+          .setColor(settings.FTeamColor)
+          .setFooter('Faithful Team', settings.FTeamIMG);
 
         m.edit(embed);
       });
---
+++ settings.js
@@ -0,0 +1,48 @@
+// Faithful Dungeons
+const FDungeonsIMG = 'https://i.imgur.com/ldI5hDM.png';
+const FDungeonsColor = '#E1631F';
+const FDungeonsID = '714910830272970834';
+
+// Faithful Mods
+const FModsIMG = 'https://i.imgur.com/scpOogK.png';
+const FModsColor = '#915E00';
+const FModsID = '748264625962877019';
+
+// Faithful Tweaks
+const FTweaksIMG = 'https://i.imgur.com/4U1ZSYT.png';
+const FTweaksColor = '#C4905A';
+const FTweaksID = '720966967325884426';
+
+// Faithful Addons
+const FAddonsIMG = 'https://i.imgur.com/OSpMYU6.png';
+const FAddonsColor = '#47E9D6';
+const FAddonsID = '614160586032414845';
+
+// Faithful Traditional
+const FTraditionalIMG = 'https://i.imgur.com/SLpji2x.png';
+const FTraditionalColor = '#FFD800';
+const FTraditionalID = '766856721712545824';
+
+// Faithful Team
+const FTeamIMG = 'https://i.imgur.com/W1pfHe0.png';
+const FTeamColor = '#FFBB00';
+
+module.exports = {
+    FDungeonsIMG,
+    FDungeonsColor,
+    FDungeonsID,
+    FModsIMG,
+    FModsColor,
+    FModsID,
+    FTweaksIMG,
+    FTweaksColor,
+    FTweaksID,
+    FAddonsIMG,
+    FAddonsColor,
+    FAddonsID,
+    FTraditionalIMG,
+    FTraditionalColor,
+    FTraditionalID,
+    FTeamIMG,
+    FTeamColor,
+}
