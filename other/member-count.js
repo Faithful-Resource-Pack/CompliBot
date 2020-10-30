3df13dbf87c6e096ffbca612143cd91a8c597f0b
Robert Reichel <robertreich1212@gmail.com>
2020-10-30 13:58:32 +0000

    fix crash when guild member update isn't on tweaks server

--- other/member-count.js
+++ other/member-count.js
@@ -1,6 +1,7 @@
 module.exports = (client) => {
 
   const updateMembers = (guild) => {
+    if (guild.channels.cache.get !== '720966967325884426') return
     const channel = guild.channels.cache.get('750638888296382504')
     channel.setName(`Member Count: ${guild.memberCount.toLocaleString()}`)
   }
