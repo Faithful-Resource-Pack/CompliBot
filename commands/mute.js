4ed99d3ed723c5b2e68fed5103c3df075ba2201d
Robert Reichel <robertreich1212@gmail.com>
2020-12-07 19:48:04 +0000

    check if the user is already muted or unmuted

--- commands/mute.js
+++ commands/mute.js
@@ -7,15 +7,16 @@ module.exports = {
 
 		if(message.member.roles.cache.find(r => r.name === "God") || message.member.roles.cache.find(r => r.name === "Moderator") || message.member.roles.cache.find(r => r.name === "Moderators") ||message.member.roles.cache.find(r => r.name === "Mods")) {
 			if (args != '') {
-				if (args == '<@' + message.author.id  + '>') message.reply('You can\'t mute yourself!')
-				else {
-					var role = message.guild.roles.cache.find(role => role.name === 'Muted');
-					var member = message.mentions.members.first();
-					member.roles.add(role);
+        var role = message.guild.roles.cache.find(r => r.name === 'Muted');
+				var member = message.mentions.members.first();
 
+				if (args == '<@' + message.author.id  + '>') return message.reply('You can\'t mute yourself!')
+        if (member.roles.cache.find(r => r.name === "Muted")) return message.reply('this user is already muted!');
+				else {
+					member.roles.add(role);
 					message.reply(args + ' is now muted.');
 				}
-			} else message.reply('Please provide a player tag!');
+			} else message.reply('Please provide a user tag!');
 		} else {
 			return message.reply(speech.BOT_NO_PERMISSION).then(msg => {
         msg.delete({timeout: 30000});
--- commands/unmute.js
+++ commands/unmute.js
@@ -7,12 +7,13 @@ module.exports = {
 
 		if(message.member.roles.cache.find(r => r.name === "God") || message.member.roles.cache.find(r => r.name === "Moderator") || message.member.roles.cache.find(r => r.name === "Moderators") ||message.member.roles.cache.find(r => r.name === "Mods")) {
 			if (args != '') {
-		
 				var role = message.guild.roles.cache.find(role => role.name === 'Muted');
 				var member = message.mentions.members.first();
-				member.roles.remove(role);
-
-				message.reply(args + ' is not muted anymore.');
+        if (!member.roles.cache.find(r => r.name === "Muted")) return message.reply('this user was never muted!');
+				else {
+          member.roles.remove(role);
+				  message.reply(args + ' is not muted anymore.');
+        }
 			} else message.reply('Please provide a player tag!');
 		} else {
 			return message.reply(speech.BOT_NO_PERMISSION).then(msg => {
