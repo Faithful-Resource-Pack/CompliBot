c5f17cf3adab69f68ee4efc98e4aa2418a42bc52
TheRolf <yann.lv@live.fr>
2021-05-08 17:57:31 +0200

    Made import build script

---
+++ helpers/firestorm/all.js
@@ -0,0 +1,8 @@
+module.exports = {
+  animations: require('./animations'),
+  contributions: require('./contributions'),
+  texture_path: require('./texture_paths'),
+  texture_use: require('./texture_use'),
+  texture: require('./texture'),
+  users: require('./users')
+}
