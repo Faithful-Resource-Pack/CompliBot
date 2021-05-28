beb411505f45acd8ea57e368e291d660ce624fe0
Julien <49886317+Juknum@users.noreply.github.com>
2021-05-28 18:47:44 +0200

    remove comments

--- functions/moderation/checkTimeout.js
+++ functions/moderation/checkTimeout.js
@@ -20,32 +20,6 @@ async function checkTimeout(client) {
 			}
 		}
 	}
-
-	/*
-	let warnList = await jsonModeration.read()
-
-	// invisible try
-	try {
-	
-	for (var i = 0; i < warnList.length; i++) {
-		if (warnList[i].timeout > 0) {
-			warnList[i].timeout = warnList[i].timeout - 30
-			if (warnList[i].timeout < 0 && warnList[i].timeout > -30) {
-				warnList[i].timeout = 0
-			}
-			if (warnList[i].timeout == 0) {
-				removeMutedRole(client, warnList[i].user)
-				warnList[i].muted = false
-			}
-		}
-	}
-
-	await jsonModeration.write(warnList)
-
-	// invisible catch
-	} catch(_error) {
-		jsonModeration.release()
-	}*/
 }
 
 exports.checkTimeout = checkTimeout
