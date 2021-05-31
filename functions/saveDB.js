cb28bd6ff782889b29d00a0ed1c1b0ecba991eeb
Julien <49886317+Juknum@users.noreply.github.com>
2021-05-31 16:48:47 +0200

    update saveDB :eyes:

--- functions/saveDB.js
+++ functions/saveDB.js
@@ -4,28 +4,20 @@ const allCollection  = require('../helpers/firestorm/all')
 const { dirname, normalize, join } = require('path')
 
 /**
- * Save the Database disant files to local, then push them to the JSON repository
+ * Save the Database distant files to local, then push them to the JSON repository (using allCollection as base)
  * @author Juknum
  * @param {String} commitMessage
  */
 async function saveDB(commitMessage) {
-  let animations = JSON.stringify(await allCollection.animations.read_raw(), null, 2)
-  let contributions = JSON.stringify(await allCollection.contributions.read_raw(), null, 2)
-  let texture = JSON.stringify(await allCollection.texture.read_raw(), null, 2)
-  let texture_path = JSON.stringify(await allCollection.texture_path.read_raw(), null, 2)
-  let texture_uses = JSON.stringify(await allCollection.texture_use.read_raw(), null, 2)
-  let users = JSON.stringify(await allCollection.users.read_raw(), null, 2)
 
   fs.mkdirSync(dirname('json/database/'), { recursive: true })
 
-  fs.writeFileSync(join(process.cwd(), normalize('json/database/animations.json')), animations, { flag: 'w', encoding: 'utf-8' })
-  fs.writeFileSync(join(process.cwd(), normalize('json/database/contributions.json')), contributions, { flag: 'w', encoding: 'utf-8' })
-  fs.writeFileSync(join(process.cwd(), normalize('json/database/texture.json')), texture, { flag: 'w', encoding: 'utf-8' })
-  fs.writeFileSync(join(process.cwd(), normalize('json/database/texture_path.json')), texture_path, { flag: 'w', encoding: 'utf-8' })
-  fs.writeFileSync(join(process.cwd(), normalize('json/database/texture_uses.json')), texture_uses, { flag: 'w', encoding: 'utf-8' })
-  fs.writeFileSync(join(process.cwd(), normalize('json/database/users.json')), users, { flag: 'w', encoding: 'utf-8' })
-
-  await githubPush('Compliance-Resource-Pack', 'JSON', 'main', commitMessage, './json/database/')
+  for (const [key, collection] of Object.entries(allCollection)) {
+    let text = JSON.stringify(await collection.read_raw(), null, 2)
+    fs.writeFileSync(join(process.cwd(), normalize('json/database/' + key + '.json')), text, { flag: 'w', encoding: 'utf-8' })
+  }
+  
+  githubPush('Compliance-Resource-Pack', 'JSON', 'main', commitMessage, './json/')
 }
 
 exports.saveDB = saveDB
--- helpers/firestorm/all.js
+++ helpers/firestorm/all.js
@@ -1,8 +1,8 @@
 module.exports = {
-  animations: require('./animations'),
+  animations:    require('./animations'),
   contributions: require('./contributions'),
-  texture_path: require('./texture_paths'),
-  texture_use: require('./texture_use'),
-  texture: require('./texture'),
-  users: require('./users')
+  texture_path:  require('./texture_paths'),
+  texture_use:   require('./texture_use'),
+  texture:       require('./texture'),
+  users:         require('./users')
 }
