c6bda2aa3ca09c6ea14ec091240c9fd2a025298f
RobertR11 <>
2021-06-01 10:19:37 +0000

    Fixed saveDB function

--- functions/saveDB.js
+++ functions/saveDB.js
@@ -10,12 +10,24 @@ const { dirname, normalize, join } = require('path')
  * @param {String} commitMessage
  */
 async function saveDB(commitMessage) {
+  const folderPath = join(
+    process.cwd(),
+    'json/database/'
+  )
 
-  fs.mkdirSync(dirname('json/database/'), { recursive: true })
+  fs.mkdirSync(folderPath,
+    { recursive: true }
+  )
 
   for (const [key, collection] of Object.entries(allCollection)) {
     let text = JSON.stringify(await collection.read_raw(), null, 2)
-    fs.writeFileSync(join(process.cwd(), normalize('json/database/' + key + '.json')), text, { flag: 'w', encoding: 'utf-8' })
+    fs.writeFileSync(join(
+      folderPath,
+      key + '.json'
+      ),
+      text,
+      { flag: 'w+', encoding: 'utf-8' }
+    )
   }
   
   githubPush('Compliance-Resource-Pack', 'JSON', 'main', commitMessage, './json/')
