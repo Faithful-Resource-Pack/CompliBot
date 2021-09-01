c3015a1c7febdf58f161f6714115269ca86e36e1
Julien Constant <49886317+Juknum@users.noreply.github.com>
2021-09-01 19:25:23 +0200

    update reactions to be a switch instead of an huge if statement

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -7,29 +7,32 @@ const { manageExtraRoles } = require('../functions/manageExtraRoles')
 
 module.exports = {
   name: 'messageReactionAdd',
-  // eslint-disable-next-line no-unused-vars
   async execute(reaction, user) {
     if (DEV) return
     if (user.bot) return
     if (reaction.message.partial) await reaction.message.fetch() // dark magic to fetch message that are sent before the start of the bot
 
-    /**
-     * NEW TEXTURE SUBMISSION
-     */
-    if (
-      reaction.message.channel.id === settings.C32_SUBMIT_TEXTURES || // c32x server
-      reaction.message.channel.id === settings.C32_SUBMIT_COUNCIL ||
-      reaction.message.channel.id === settings.C32_SUBMIT_REVOTE ||
-      reaction.message.channel.id === settings.C32_RESULTS ||
-
-      reaction.message.channel.id === settings.C64_SUBMIT_TEXTURES || // c64x server
-      reaction.message.channel.id === settings.C64_SUBMIT_COUNCIL ||
-      reaction.message.channel.id === settings.C64_SUBMIT_REVOTE ||
-      reaction.message.channel.id === settings.C64_RESULTS ||
-
-      reaction.message.channel.id === settings.CDUNGEONS_SUBMIT // dungeons server REMOVE THIS ASAP
-    ) editSubmission(client, reaction, user)
-
-    if (reaction.message.channel.id === settings.CEXTRAS_ROLES) manageExtraRoles(client, reaction, user)
+    switch (reaction.message.channel.id) {
+      // TEXTURES SUBMISSIONS
+      case settings.C32_SUBMIT_TEXTURES:
+      case settings.C32_SUBMIT_COUNCIL:
+      case settings.C32_SUBMIT_REVOTE:
+      case settings.C32_RESULTS:
+      case settings.C64_SUBMIT_TEXTURES:
+      case settings.C64_SUBMIT_COUNCIL:
+      case settings.C64_SUBMIT_REVOTE:
+      case settings.C64_RESULTS:
+        editSubmission(client, reaction, user)
+        break;
+      
+      // EXTRA ROLES MANAGER
+      case settings.CEXTRAS_ROLES:
+        manageExtraRoles(client, reaction, user)
+        break;
+      
+      // Not setup
+      default:
+        break;
+    }
   }
 }
