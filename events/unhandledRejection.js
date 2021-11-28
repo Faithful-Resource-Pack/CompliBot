9071bd86640b16b91b79ec7c867a61058f97001c
TheRolf <yann.lv@live.fr>
2021-11-28 10:49:39 +0100

    Little helpus command

--- events/unhandledRejection.js
+++ events/unhandledRejection.js
@@ -9,16 +9,24 @@ const LOG_DEV = ((process.env.LOG_DEV.toLowerCase() || 'false') == 'true')
  * @param {Discord.Client} client Discord client treating the information
  * @param {Error|any} reason The object with which the promise was rejected
  * @param {Promise} promise The rejected promise
+ * @param {import('discord.js').Message} originMessage Origin user message
  */
-module.exports = function(client, reason, promise) {
-  const settings = require('./resources/settings.json')
+module.exports = function(client, reason, promise, originMessage) {
+  const settings = require('../resources/settings.json')
 
 	if (DEV) return console.trace(reason.stack || reason)
 
 	const channel = client.channels.cache.get(LOG_DEV ? '875301873316413440' : '853547435782701076')
+
+	let description = `\`\`\`fix\n${reason.stack || JSON.stringify(reason)}\`\`\``
+	
+	if(originMessage !== undefined && originMessage.url !== undefined) {
+		description = 'Coming from [this message](' + originMessage.url + ')\n' + description
+	}
+
 	const embed = new MessageEmbed()
 		.setTitle('Unhandled Rejection')
-		.setDescription(`\`\`\`fix\n${reason.stack || JSON.stringify(reason)}\`\`\``)
+		.setDescription(description)
 		.setColor(settings.colors.red)
 		.setTimestamp()
 
