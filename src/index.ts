9f4f5c988cf02739c9273d8bbe15011574bdfee1
Robert Reichel <robertreich1212@gmail.com>
2021-12-24 20:07:45 +0100

    disable mentioning everyone and improve reaction speeds

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 import { Intents } from 'discord.js';
 import Client from './Client';
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] }).init();
+const client = new Client({
+    allowedMentions: { parse: ['users', 'roles'], repliedUser: false }, // remove this line to die instantly ~JackDotJS 2021
+    restTimeOffset: 0,
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+}).init();
