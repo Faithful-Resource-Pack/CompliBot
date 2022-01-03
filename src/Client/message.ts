9a06d09bc9378ddc4f8d66c0103180b6dca613a8
Julien Constant <julienconstant190@gmail.com>
2022-01-03 13:08:13 +0100

    final fixes for choiceMenu (now renamed to menu)

--- src/Client/message.ts
+++ src/Client/message.ts
@@ -1,12 +1,21 @@
 import { Message, MessageEmbed } from 'discord.js';
 import { Config } from '~/Interfaces';
 import ConfigJson from '@/config.json';
+import Menu from '~/Helpers/menu';
 
 declare module 'discord.js' {
-  interface Message {
-    config: Config;
-    warn(text: string): void;
-  }
+	interface Message {
+		config: Config;
+		menu: Menu;
+		setMenu(choiceType: 'texture' | 'imageOptions', id: string): Menu;
+		warn(text: string): void;
+	}
+}
+
+Message.prototype.menu = undefined;
+Message.prototype.setMenu = function (choiceType: 'texture' | 'imageOptions', id: string) {
+	this.menu = new Menu(this, choiceType, id)
+	return this.menu;
 }
 
 // access to config file trough the message
@@ -16,16 +25,17 @@ Message.prototype.config = ConfigJson;
  *  Warn the message by replying to it with an embed
  *  @author Juknum
  */
-Message.prototype.warn = function(text: string) {
-  const embed = new MessageEmbed()
-    .setColor(this.config.colors.red)
-    .setThumbnail('https://database.compliancepack.net/images/bot/warning.png')
-    .setTitle('Action failed')
-    .setDescription(text)
-    .setFooter(`Type ${this.config.prefix}help to get more information about commands`, this.client.user.displayAvatarURL()); // todo : deprecated
+Message.prototype.warn = function (text: string) {
+	const embed = new MessageEmbed()
+		.setColor(this.config.colors.red)
+		.setThumbnail('https://database.compliancepack.net/images/bot/warning.png')
+		.setTitle('Action failed')
+		.setDescription(text)
+		.setFooter({ text: `Type ${this.config.prefix}help to get more information about commands`, iconURL: this.client.user.displayAvatarURL() });
 
-  if (this.deleted) this.channel.send({ embeds: [embed] });
-  else this.reply({ embeds: [embed] });
+	try { // test if message is deleted or not
+		this.reply({ embeds: [embed] });
+	} catch (err) { this.channel.send({ embeds: [embed] }) };
 }
 
 export default Message;
