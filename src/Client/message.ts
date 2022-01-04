1a4562a7522e8443239d808da684aafc92de6f16
Spoon8285 <Spoonboimail@gmail.com>
2022-01-04 20:15:35 +0000

    Disappearing warns
    Warn function on Message.prototype now takes in an optional parameter of "disappearing" which when defined as true deletes the message 30s after it is sent.

--- src/Client/message.ts
+++ src/Client/message.ts
@@ -4,6 +4,7 @@ import ConfigJson from '@/config.json';
 import Menu from '~/Helpers/menu';
 import { ids } from '~/Helpers/emojis';
 import tokens from '@/tokens.json';
+import { stringify } from 'querystring';
 
 declare module 'discord.js' {
 	interface Message {
@@ -11,8 +12,8 @@ declare module 'discord.js' {
 		config: Config;
 		menu: Menu;
 		setMenu(choiceType: 'texture' | 'imageOptions', id: string): Menu;
-		warn(text: string): void;
-		deleteReact(options: { authorMessage: Message, previousMessage?: Message, deleteAuthorMessage: boolean }): void;
+		warn(text: string, disappearing?: boolean): void;
+		deleteReact(options: { authorMessage: Message; previousMessage?: Message; deleteAuthorMessage: boolean }): void;
 	}
 }
 
@@ -22,49 +23,63 @@ Message.prototype.tokens = tokens;
  * Add a trash can emote and await of user interaction, if used, the message is deleted
  * - does nothing if it's DM
  */
-Message.prototype.deleteReact = async function (options: { authorMessage: Message, previousMessage?: Message, deleteAuthorMessage: boolean }) {
+Message.prototype.deleteReact = async function (options: { authorMessage: Message; previousMessage?: Message; deleteAuthorMessage: boolean }) {
 	if (this.channel.type === 'DM') return;
 
 	// react using the trash can emoji
-	await this.react(ids.delete).catch((err) => { console.trace(err) })
+	await this.react(ids.delete).catch((err) => {
+		console.trace(err);
+	});
 
 	// filter to get the right user
 	const filter = (reaction, user) => {
-		if (options.previousMessage) return !user.bot && ids.delete === reaction.emoji.id && user.id === options.previousMessage.author.id
-		else return !user.bot && ids.delete === reaction.emoji.id && user.id === options.authorMessage.author.id
-	}
+		if (options.previousMessage) return !user.bot && ids.delete === reaction.emoji.id && user.id === options.previousMessage.author.id;
+		else return !user.bot && ids.delete === reaction.emoji.id && user.id === options.authorMessage.author.id;
+	};
 
 	// await for reaction for 1 minute long
 	this.awaitReactions({ filter: filter, max: 1, time: 60000, errors: ['time'] })
 		.then(async () => {
-			await this.delete().catch((err) => { console.trace(err) })
+			await this.delete().catch((err) => {
+				console.trace(err);
+			});
 
 			if (options.deleteAuthorMessage === true) {
-				if (options.previousMessage) await options.previousMessage.delete().catch((err) => { console.trace(err) })
-				else await options.authorMessage.delete().catch((err) => { console.trace(err) })
+				if (options.previousMessage)
+					await options.previousMessage.delete().catch((err) => {
+						console.trace(err);
+					});
+				else
+					await options.authorMessage.delete().catch((err) => {
+						console.trace(err);
+					});
 			}
 		})
 		.catch(async () => {
 			// on timeout:
-			const reaction = this.reactions.cache.get(ids.delete)
-			if (reaction) await reaction.remove().catch((err) => { console.trace(err) });
-		})
-}
+			const reaction = this.reactions.cache.get(ids.delete);
+			if (reaction)
+				await reaction.remove().catch((err) => {
+					console.trace(err);
+				});
+		});
+};
 
 Message.prototype.menu = undefined;
 Message.prototype.setMenu = function (choiceType: 'texture' | 'imageOptions', id: string) {
-	this.menu = new Menu(this, choiceType, id)
+	this.menu = new Menu(this, choiceType, id);
 	return this.menu;
-}
+};
 
 // access to config file trough the message
 Message.prototype.config = ConfigJson;
 
 /**
  *  Warn the message by replying to it with an embed
- *  @author Juknum
+ *  @author Juknum & Nick
+ *  @param {boolean} disappearing Optional bool. If undefined or false it wont delete the warning. If true it will  delete in 30s.
  */
-Message.prototype.warn = function (text: string) {
+Message.prototype.warn = async function (text: string, disappearing?: boolean) {
 	const embed = new MessageEmbed()
 		.setColor(this.config.colors.red)
 		.setThumbnail(`${this.config.images}warning.png`)
@@ -72,10 +87,20 @@ Message.prototype.warn = function (text: string) {
 		.setDescription(text)
 		.setFooter({ text: `Type ${this.tokens.prefix}help to get more information about commands`, iconURL: this.client.user.displayAvatarURL() });
 
-	this.reply({ embeds: [embed] })
-		.then((sentMessage: Message) => { sentMessage.deleteReact({ authorMessage: this, deleteAuthorMessage: true }) })
-		.catch(err => { this.channel.send({ embeds: [embed] }) })
+	const replyMsg = await this.reply({ embeds: [embed] });
 
-}
+	if (!disappearing) {
+		try {
+			replyMsg.deleteReact({ authorMessage: this, deleteAuthorMessage: true });
+		} catch (err) {
+			this.channel.send({ embeds: [embed] });
+		}
+	} else {
+		setTimeout(() => {
+			replyMsg.delete();
+			this.delete();
+		}, 3000); //deletes the message after 30s
+	}
+};
 
 export default Message;
--- src/Functions/unhandledRejection.ts
+++ src/Functions/unhandledRejection.ts
@@ -1,60 +1,68 @@
-import { MessageAttachment, MessageEmbed, TextChannel } from "discord.js";
-import Client from "~/Client"
-import Message from "~/Client/message"
+import { MessageAttachment, MessageEmbed, TextChannel } from 'discord.js';
+import Client from '~/Client';
+import Message from '~/Client/message';
 import fs from 'fs';
 
 const randomSentences: Array<string> = [
-  'Oh no, not again!',
-  'Well, it\'s unexpected...',
-  'OOPS, sorry, my bad!',
-  'I thought TS > JS was true...',
-  'This one is going to be a nightmare to solve!',
-  'Please, don\'t blame me, I try my best. Each day.',
-  'Like humans, I have some errors',
-  'Don\'t be sad, have a hug <3',
-  'SCHEISE',
-  'oh.',
-  'Another one! DJ Khaleeeeed!',
-  'I just don\'t know what went wrong :(',
-  'My bad.',
-  'Hold my beer.'
-]
+	'Oh no, not again!',
+	"Well, it's unexpected...",
+	'OOPS, sorry, my bad!',
+	'I thought TS > JS was true...',
+	'This one is going to be a nightmare to solve!',
+	"Please, don't blame me, I try my best. Each day.",
+	'Like humans, I have some errors',
+	"Don't be sad, have a hug <3",
+	'SCHEISE',
+	'oh.',
+	'Another one! DJ Khaleeeeed!',
+	"I just don't know what went wrong :(",
+	'My bad.',
+	'Hold my beer.',
+];
 
 export const unhandledRejection: Function = async (client: Client, reason: any) => {
-  const channel = client.channels.cache.get(client.config.channels.error) as TextChannel;
+	const channel = client.channels.cache.get(client.config.channels.error) as TextChannel;
 
-  const embed = new MessageEmbed()
-    .setTitle('Unhandled Rejection')
-    .setThumbnail(`${client.config.images}error.png`)
-    .setColor(client.config.colors.red)
-    .addField('Last message(s) received:', `${client.getLastMessages().map((message: Message, index) => `[Message ${index + 1}](${message.url}) - ${message.channel.type === 'DM' ? 'DM' : `<#${message.channel.id}>`}`).join('\n')}`, false)
-    .setTimestamp()
-    .setFooter({ text: client.user.tag, iconURL: client.user.avatarURL() })
+	const embed = new MessageEmbed()
+		.setTitle('Unhandled Rejection')
+		.setThumbnail(`${client.config.images}error.png`)
+		.setColor(client.config.colors.red)
+		.addField(
+			'Last message(s) received:',
+			`${client
+				.getLastMessages()
+				.map((message: Message, index) => `[Message ${index + 1}](${message.url}) - ${message.channel.type === 'DM' ? 'DM' : `<#${message.channel.id}>`}`)
+				.join('\n')}`,
+			false,
+		)
+		.setTimestamp()
+		.setFooter({ text: client.user.tag, iconURL: client.user.avatarURL() });
 
-  const logTemplate = fs.readFileSync(__dirname + '/unhandledRejection.log', { encoding: 'utf-8' })
-  const messageTemplate = logTemplate.match(new RegExp(/\%messageStart%([\s\S]*?)%messageEnd/))[1]; // get message template
+	const logTemplate = fs.readFileSync(__dirname + '/unhandledRejection.log', { encoding: 'utf-8' });
+	const messageTemplate = logTemplate.match(new RegExp(/\%messageStart%([\s\S]*?)%messageEnd/))[1]; // get message template
 
-  const t = Math.floor(Math.random() * randomSentences.length);
-  let log = logTemplate
-    .replace('%date%', new Date().toUTCString())
-    .replace('%stack%', reason.stack || JSON.stringify(reason))
-    .replace('%randomSentence%', randomSentences[t])
-    .replace('%randomSentenceUnderline%', '-'.repeat(randomSentences[t].length));
+	const t = Math.floor(Math.random() * randomSentences.length);
+	let log = logTemplate
+		.replace('%date%', new Date().toUTCString())
+		.replace('%stack%', reason.stack || JSON.stringify(reason))
+		.replace('%randomSentence%', randomSentences[t])
+		.replace('%randomSentenceUnderline%', '-'.repeat(randomSentences[t].length));
 
-  log = log.split('%messageStart%')[0] // remove message template
+	log = log.split('%messageStart%')[0]; // remove message template
 
-  client.getLastMessages().forEach((message: Message, index) => {
-    log += messageTemplate
-      .replace('%messageIndex%', index.toString())
-      .replace('%messageCreatedTimestamp%', message.createdTimestamp.toString())
-      .replace('%messageURL%', message.url)
-      .replace('%messageChannelType%', message.channel.type)
-      .replace('%messageContent%', message.content);
-  });
+	client.getLastMessages().forEach((message: Message, index) => {
+		log += messageTemplate
+			.replace('%messageIndex%', index.toString())
+			.replace('%messageCreatedTimestamp%', message.createdTimestamp.toString())
+			.replace('%messageURL%', message.url)
+			.replace('%messageChannelType%', message.channel.type)
+			.replace('%messageContent%', message.content);
+	});
 
-  const buffer = Buffer.from(log, 'utf8');
-  const attachment = new MessageAttachment(buffer, 'stack.log');
+	const buffer = Buffer.from(log, 'utf8');
+	const attachment = new MessageAttachment(buffer, 'stack.log');
 
-  await channel.send({ embeds: [embed] }).catch(console.error);
-  await channel.send({ files: [attachment] }).catch(console.error);
-}
+	await channel.send({ embeds: [embed] }).catch(console.error);
+	await channel.send({ files: [attachment] }).catch(console.error);
+	console.error(reason);
+};
