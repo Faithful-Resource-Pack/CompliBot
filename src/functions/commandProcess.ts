f77b1d46417ef3bedac65c1710016a480bf81462
Julien Constant <julienconstant190@gmail.com>
2022-01-07 00:23:23 +0100

    Bot won't crash anymore if `commandsProcessed.json` doesn't exist / has been deleted

--- src/functions/commandProcess.ts
+++ src/functions/commandProcess.ts
@@ -8,10 +8,27 @@ const COUNTER_FOLDER = path.resolve(__dirname, '../json/');
 const COUNTER_FILE_PATH = path.resolve(COUNTER_FOLDER, 'commandsProcessed.json');
 const SAVE_EVERY = 20; // save file every n commands triggered
 
-const jsonString = fs.readFileSync(COUNTER_FILE_PATH).toString();
-const commandsObj = JSON.parse(jsonString);
+function validateDirectory(): string {
+	let jsonString: string;
 
-let commandsProcessed: { [commandName: string]: number } = JSON.parse(fs.readFileSync(COUNTER_FILE_PATH).toString());
+	try {
+		jsonString = fs.readFileSync(COUNTER_FILE_PATH).toString();
+	}
+	catch (err) { // when file/folder doesn't exist/is invalid
+		// if folder doesn't exist
+		if (!existsSync(COUNTER_FOLDER)) fs.mkdirSync(COUNTER_FOLDER, { recursive: true });
+
+		// create file
+		fs.writeFileSync(COUNTER_FILE_PATH, "{}") // empty object to avoid parse errors
+
+		return validateDirectory();
+	}
+
+	return jsonString;
+}
+
+const commandsObj = JSON.parse(validateDirectory());
+let commandsProcessed: { [commandName: string]: number } = JSON.parse(validateDirectory());
 
 export function init(client: ExtendedClient) {
 	client.commands.forEach((command) => {
@@ -24,12 +41,10 @@ export var getUsage = function (commandName: string) {
 };
 
 export function increase(name: string) {
-	if (commandsProcessed[name] == undefined) commandsProcessed[name] = 1;
-	else {
-		commandsProcessed[name]++;
-	}
+	commandsProcessed[name] = (commandsProcessed[name] == undefined) ? 1 : commandsProcessed[name] + 1;
 
 	if (commandsProcessed[name] == 1 || commandsProcessed[name] % SAVE_EVERY == 0) {
+		validateDirectory(); // use to test if the file hasn't been deleted after bot start.
 		fs.writeFileSync(COUNTER_FILE_PATH, JSON.stringify(commandsProcessed));
 	}
 }
