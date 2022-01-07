54776f7d54ed649a35e96910068b79e78cc0cb87
Julien Constant <julienconstant190@gmail.com>
2022-01-07 02:35:06 +0100

    slash command for guidelines

---
+++ src/Slash Commands/Compliance/guidelines.ts
@@ -0,0 +1,14 @@
+import { SlashCommand } from "~/Interfaces/slashCommand";
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandInteraction } from "discord.js";
+
+export const command: SlashCommand = {
+	dev: true,
+	data: new SlashCommandBuilder()
+		.setName("guidelines")
+		.setDescription("Shows the guidelines for the Compliance Resource Pack.")
+	,
+	execute: async (interaction: CommandInteraction) => {
+		await interaction.reply({ content: "https://docs.compliancepack.net/pages/textures/texturing-guidelines" })
+	}
+};
