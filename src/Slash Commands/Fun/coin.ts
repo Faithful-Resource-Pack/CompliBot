5cb9a271b90ca9d1b47cd0207e242d84bd9c682e
Julien Constant <julienconstant190@gmail.com>
2022-01-10 01:02:51 +0100

    added `/coin` as a slash command

---
+++ src/Slash Commands/Fun/coin.ts
@@ -0,0 +1,19 @@
+import { SlashCommand } from "~/Interfaces/slashCommand";
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandInteraction } from "discord.js";
+import Client from "~/Client";
+
+export const command: SlashCommand = {
+  data: new SlashCommandBuilder()
+    .setName("coin")
+    .setDescription("Flip a coin. Will it be heads? Will it be tails? Who knows?")
+  ,
+  execute: (interaction: CommandInteraction, client: Client) => {
+    const res = Math.round(Math.random() * 100) / 100; // round to 2 decimal places;
+
+    /**
+     * Value is rounded to *really* being able of landing edge (instead of having 1 in 18,446,744,073,709,551,616 of landing edge)
+     */
+    interaction.reply({ content: `${res > 0.5 ? 'Heads!' : (res < 0.5 ? 'Tails!' : 'Edge???!!!')}` })
+  }
+}
