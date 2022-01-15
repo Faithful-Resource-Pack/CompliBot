c84320df82e7da0dd4382d27138efe997cf36165
Spoon8285 <Spoonboimail@gmail.com>
2022-01-15 23:11:38 +0000

    Update stringKeys.ts

--- src/Interfaces/stringKeys.ts
+++ src/Interfaces/stringKeys.ts
@@ -1,2 +1,7 @@
-export type key = 'Error.General' | 'Error.NotFound' | 'Error.DevBad' | 'Args.Invalid' | 'Args.Insufficient';
+import { readFileSync } from "fs";
+import path from "path";
+
+export type key = typeof rawKeys;
+//reads the keys from .keys and adds them
+const rawKeys = readFileSync(path.join(__dirname, "../../lang/.keys"), 'utf-8').split('\n').slice(1).join('\n').replaceAll(/(\n|\r)/, "").split(",");
 export default key;
