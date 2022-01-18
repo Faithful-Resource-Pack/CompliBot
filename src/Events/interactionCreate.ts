3dc3e3e5528d712441d91281a3d9cde3cc81cad6
Julien Constant <julienconstant190@gmail.com>
2022-01-18 23:52:16 +0100

    split interaction event into multiple events

--- src/Events/interactionCreate.ts
+++ src/Events/interactionCreate.ts
@@ -1,32 +1,14 @@
-import { Collection, Interaction } from 'discord.js';
-import { increase } from '@src/Functions/commandProcess';
+import Client from '@src/Client';
+import { Interaction } from 'discord.js';
 import { Event } from '@src/Interfaces';
-import { SlashCommandI } from '@src/Interfaces/slashCommand';
 
 export const event: Event = {
 	name: 'interactionCreate',
-	run: async (client, interaction: Interaction) => {
-		if (interaction.isCommand()) {
-			const { commandName } = interaction;
+	run: async (client: Client, interaction: Interaction) => {
+		if (!interaction.inGuild()) return;
 
-			if (!client.slashCommands.has(commandName)) return;
-			const command = client.slashCommands.get(commandName);
-
-			// todo: add verification that execute is instanceof Collection<string, SlashCommandI>
-
-			try {
-				// test if it has a subcommand
-				interaction.options.getSubcommand();
-				await (command.execute as Collection<string, SlashCommandI>).get(interaction.options.getSubcommand())(interaction, client);
-				increase((command.data as SlashCommandI).name);
-			} catch (err) {
-
-				// not a subcomand
-				try {
-					await (command.execute as SlashCommandI)(interaction, client);
-					increase((command.data as SlashCommandI).name);
-				} catch (err) { console.error(err); } // weird stuff happening
-			}
-		}
+		if (interaction.isCommand()) client.emit("slashCommandUsed", (client as Client, interaction));
+		if (interaction.isButton()) client.emit("useButton", (client as Client, interaction));
+		if (interaction.isSelectMenu()) client.emit("useSelectMenu", (client as Client, interaction));
 	},
 };
--- src/Interfaces/event.ts
+++ src/Interfaces/event.ts
@@ -1,11 +1,17 @@
 import Client from '@src/Client';
 import { ClientEvents } from 'discord.js';
 
+interface CustomEvents extends ClientEvents {
+	slashCommandUsed: any;
+	buttonUsed: any;
+	selectMenuUsed: any;
+}
+
 interface Run {
 	(client: Client, ...args: any[]);
 }
 
 export interface Event {
-	name: keyof ClientEvents;
+	name: keyof CustomEvents;
 	run: Run;
 }
