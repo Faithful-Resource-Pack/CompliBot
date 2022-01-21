0cb51cf3a3e0df977ecce9e22a8b2adc3e9f7f1e
Robert <contact.e3nk6@aleeas.com>
2022-01-21 17:55:22 +0100

    allow mods to use ban command

--- commands/Moderation/ban.js
+++ commands/Moderation/ban.js
@@ -17,7 +17,7 @@ module.exports = {
 	syntax: `${prefix}ban <@user> <reason>`,
 	example: `${prefix}ban @RobertR11#7841 breaking rule 2`,
 	async execute(client, message, args) {
-		if (!message.member.roles.cache.some(role => role.name.includes("Administrator") || role.id === '747839021421428776')) return warnUser(message, strings.command.no_permission)
+		if (!message.member.roles.cache.some(role => role.name.includes("Administrator") || role.name.includes("Moderator") || role.id === '747839021421428776')) return warnUser(message, strings.command.no_permission)
 
 		const member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
 		const reason = args.slice(1).join(' ') || 'Not Specified'
