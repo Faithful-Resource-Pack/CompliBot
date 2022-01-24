20fe8db4028db631b30a925f6418a0ddfc3966a8
Julien Constant <julienconstant190@gmail.com>
2022-01-24 01:01:03 +0100

    Added /license command

---
+++ src/Slash Commands/Compliance/license.ts
@@ -0,0 +1,13 @@
+import { SlashCommand } from "@src/Interfaces/slashCommand";
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandInteraction } from "discord.js";
+
+export const command: SlashCommand = {
+	permissions: undefined,
+	data: new SlashCommandBuilder()
+		.setName("license")
+		.setDescription("Shows the license for Compliance Resources Packs."),
+	execute: (interaction: CommandInteraction) => {
+		interaction.reply({ content: "https://www.compliancepack.net/license" });
+	},
+};
