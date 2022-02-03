623f9a353a344dbb1ac77424b0105ad6bf4a14a1
Julien Constant <julienconstant190@gmail.com>
2022-02-03 03:58:12 +0100

    add default footer for embeds

--- src/Client/embed.ts
+++ src/Client/embed.ts
@@ -1,3 +1,5 @@
+
+import ConfigJson from "@/config.json";
 import { APIEmbed } from "discord-api-types";
 import { MessageEmbed, MessageEmbedOptions } from "discord.js";
 
@@ -5,6 +7,7 @@ class ExtendedEmbed extends MessageEmbed {
 	constructor(data?: MessageEmbed | MessageEmbedOptions | APIEmbed) {
 		super(data);
 		this.setColor("BLURPLE");
+		this.setFooter({ text: 'CompliBot', iconURL: ConfigJson.images + 'monochrome_logo.png' })
 	}
 }
 
