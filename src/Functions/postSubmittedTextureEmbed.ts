a2e620148e3c9cb09964baf83055479b8f4f0135
Julien Constant <julienconstant190@gmail.com>
2022-02-20 19:09:33 +0100

    WIP

--- src/Functions/postSubmittedTextureEmbed.ts
+++ src/Functions/postSubmittedTextureEmbed.ts
@@ -3,6 +3,8 @@ import { MessageAttachment } from "discord.js";
 import { Config } from "@interfaces/config";
 import ConfigJson from "@/config.json";
 import { magnifyAttachment } from "./canvas/magnify";
+import axios from "axios";
+import { minecraftSorter } from "./minecraftSorter";
 
 export const addMinutes = (d: Date, minutes: number): Date => {
   return new Date(d.getTime() + minutes * 60000);
@@ -19,12 +21,16 @@ export const postSubmitedTextureEmbed = async (message: Message, texture: any, f
     .addField('Tags', texture.tags.join(', '))
     .addField('Contributor(s)', `<@!${mentions.join('>\n<@!')}>`, true)
     .addField('Resource Pack', `\`${Object.keys(config.submitChannels).filter(key => config.submitChannels[key] === message.channel.id)[0]}\``, true)
-    .addField('Pending until', `<t:${(addMinutes(new Date(), 4320).getTime() / 1000).toFixed(0)}>`, true) // 4320 is 3 days
-    .setImage(file.url)
+    .addField('Status', `Waiting votes`)
+    .addField('Until', `<t:${(addMinutes(new Date(), 4320).getTime() / 1000).toFixed(0)}>`, true) // 4320 is 3 days
+    .setImage(file.proxyURL)
     .setThumbnail('attachment://magnified.png')
 
   if (message.content !== '' || message.content !== undefined) embed.setDescription(message.content);
 
-  files.push(await magnifyAttachment({ url: file.url, name: 'magnified.png' }));
-  message.channel.send({ embeds: [embed], files: [file, ...files]});
+  files.push(await magnifyAttachment({ 
+    url: (await axios.get(`${config.apiUrl}textures/${texture.id}/url/default/${texture.paths[0].versions.sort(minecraftSorter).reverse()[0]}`)).request.res.responseUrl, 
+    name: 'magnified.png' 
+  }));
+  message.channel.send({ embeds: [embed], files: [...files]});
 }
