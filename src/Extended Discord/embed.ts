6d8b9f5fde2e63016e4ecbe7ff58c8f813997f2b
Julien Constant <julienconstant190@gmail.com>
2022-02-23 23:18:30 +0100

    avoid data override when copying embeds

--- src/Extended Discord/embed.ts
+++ src/Extended Discord/embed.ts
@@ -5,6 +5,8 @@ import { MessageEmbed, MessageEmbedOptions } from "discord.js";
 class ExtendedEmbed extends MessageEmbed {
 	constructor(data?: MessageEmbed | MessageEmbedOptions | APIEmbed) {
 		super(data);
+		if (data) return; // do not override existing data
+		
 		this.setColor("BLURPLE");
 		this.setFooter({ text: "CompliBot", iconURL: ConfigJson.icon });
 	}
