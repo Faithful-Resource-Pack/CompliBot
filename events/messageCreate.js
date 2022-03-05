f2d4212672d31c249ff4808c8de2ed9bd479c9f1
Robert Reichel <robertreich1212@gmail.com>
2022-03-05 15:32:21 +0100

    remove features implemented in rewrite

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -111,30 +111,6 @@ module.exports = {
       if (DEV) return
 
       /**
-       * EASTER EGGS
-       */
-      //disabled because they exist in beta bot
-      
-//       if (message.content.includes('(╯°□°）╯︵ ┻━┻')) return await message.reply({ content: '┬─┬ ノ( ゜-゜ノ) calm down bro' })
-      
-//       if (message.content.toLowerCase() === 'mhhh') {
-//         const embed = new MessageEmbed()
-//           .setDescription('```Uh-oh moment```')
-//           .setColor(settings.colors.blue)
-//           .setFooter('Swahili → English', client.user.displayAvatarURL())
-//         let msgEmbed = await message.reply({ embeds: [embed] })
-//         return addDeleteReact(msgEmbed, message)
-//       }
-
-//       if (message.content.toLowerCase() === 'hello there') {
-//         let msgEmbed
-//         if (Math.floor(Math.random() * Math.floor(5)) != 1) msgEmbed = await message.reply({ content: 'https://media1.tenor.com/images/8dc53503f5a5bb23ef12b2c83a0e1d4d/tenor.gif' })
-//         else msgEmbed = await message.reply({ content: 'https://preview.redd.it/6n6zu25c66211.png?width=960&crop=smart&auto=webp&s=62024911a6d6dd85f83a2eb305df6082f118c8d1' })
-
-//         return addDeleteReact(msgEmbed, message)
-//       }
-
-      /**
        * TEXTURE ID QUOTE
        * when someone type [#1234], send an embed with the given texture id
        * @author Juknum
