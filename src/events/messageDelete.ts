16c547338fcffaba90344aceda869bfe5d92e039
Robert Reichel <robertreich1212@gmail.com>
2022-04-11 15:40:22 +0200

    can we switch back to javascript

--- src/events/messageDelete.ts
+++ src/events/messageDelete.ts
@@ -6,7 +6,7 @@ import { colors } from "@helpers/colors";
 export const event: Event = {
 	name: "messageDelete",
 	run: async (client: Client, message: Message) => {
-		if (message.author.bot) return;
+		if (message.author && message.author.bot) return;
 
 		//! do not remove, 'force' message to be casted (break if removed)
 		//"I need a "typescript issue" gif" ~RobertR11 2022
