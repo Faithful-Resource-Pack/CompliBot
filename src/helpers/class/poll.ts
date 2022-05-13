f07e32dd0070541d58ad279db7f64194e304a9ed
Robert <contact.e3nk6@aleeas.com>
2022-05-13 22:42:42 +0200

    fixing the poll fix

--- src/helpers/class/poll.ts
+++ src/helpers/class/poll.ts
@@ -74,7 +74,8 @@ export class Poll extends TimedEmbed {
 			return field;
 		});
 
-		if (message.thread && !message.thread.archived) await message.edit({ embeds: [embed], components: [...components] });
+		if (message.thread && message.thread.archived) return;
+		await message.edit({ embeds: [embed], components: [...components] });
 		return;
 	}
 
