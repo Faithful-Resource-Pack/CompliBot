922f5ce6e098a7083c4ad9267896cab55311fd45
Robert <contact.e3nk6@aleeas.com>
2022-06-21 20:42:40 +0200

    Revert "remove useless commands and commands implemented in ts version"
    This reverts commit c9b721754f0424438f1c597a14112c304671fef2.

---
+++ commands/Bot/info.js
@@ -0,0 +1,57 @@
+const prefix = process.env.PREFIX
+
+const Discord = require("discord.js")
+const settings = require('../../resources/settings.json')
+const strings = require('../../resources/strings.json')
+
+module.exports = {
+	name: 'info',
+	aliases: ['botinfo'],
+	description: strings.command.description.info,
+	category: 'Bot',
+	guildOnly: false,
+	uses: strings.command.use.anyone,
+	syntax: `${prefix}info`,
+	async execute(client, message, args) {
+		if (message.channel.type !== 'DM') await message.reply({ content: 'Please check your dm\'s!' });
+
+		let seconds = Math.floor(message.client.uptime / 1000);
+		let minutes = Math.floor(seconds / 60);
+		let hours = Math.floor(minutes / 60);
+		let days = Math.floor(hours / 24);
+
+		let tseconds = 'seconds';
+		let tminutes = 'minutes';
+		let thours = 'hours';
+		let tdays = 'days';
+
+		seconds %= 60;
+		minutes %= 60;
+		hours %= 24;
+
+		if (seconds == 1) tseconds = 'second';
+		if (minutes == 1) tminutes = 'minute';
+		if (hours == 1) thours = 'hour';
+		if (days == 1) thours = 'day';
+
+		const embed = new Discord.MessageEmbed()
+			.setTitle(`${client.user.username} Info:`)
+			.setThumbnail(client.user.displayAvatarURL())
+			.setColor(settings.colors.blue)
+			.setDescription('This is the official bot of all Faithful Resource Pack discords, developed by the Faithful Team!')
+			.addFields(
+				{ name: '\u200B', value: '**Features:**' },
+				{ name: '🔍 Image Resizing', value: 'With the `/magnify <factor>` command you can resize images by a certain factor! \nKeep in mind, this feature was made for textures, other images may look weird.' },
+				{ name: '🔍 Image Tiling', value: 'With the `/tile` command you can tile mainly block textures to a 3x3 field!' },
+				{ name: '🎨 Texture Viewer', value: 'With `/texture <resolution> <texture name>` you can quickly get send a texture and its information!' },
+				{ name: '❗ Modping', value: 'With the `/modping` command you can let me ping all mods online on one of our servers to get help.' },
+				{ name: '🗺️ soon™️: Translations', value: 'Soon you can translate any message when typing `/translate *to language* *your text*` or by reacting with a flag emoji to an existing message!' },
+				{ name: 'And more!', value: 'If you need a full list of all my commands, then use `/help`!' },
+				{ name: '\u200B', value: '\n\nI am completely open source!\nYou can find me by [clicking here](https://github.com/Faithful-Resource-Pack/Discord-Bot).' },
+			)
+			.setFooter(`Bot Uptime: ${days} ${tdays}, ${hours} ${thours}, ${minutes} ${tminutes}, ${seconds} ${tseconds}`)
+
+		if (message.channel.type !== 'DM') await message.author.send({ embeds: [embed] });
+		else await message.reply({ embeds: [embed] });
+	}
+};
---
+++ commands/Fun/bean.js
@@ -0,0 +1,37 @@
+const prefix = process.env.PREFIX
+
+const strings = require('../../resources/strings.json')
+const settings = require('../../resources/settings.json')
+
+const { MessageEmbed } = require('discord.js')
+const { warnUser } = require('../../helpers/warnUser')
+
+module.exports = {
+	name: 'bean',
+	description: strings.command.description.bean,
+	category: 'Fun',
+	guildOnly: true,
+	uses: strings.command.use.mods,
+	syntax: `${prefix}bean <@user> <reason>`,
+	example: `${prefix}bean @Sei#0721 spilling the beans`,
+	async execute(client, message, args) {
+		if (!message.member.roles.cache.some(role => role.name.includes("Administrator") || role.name.includes("Moderator") || role.id === '747839021421428776')) return warnUser(message, strings.command.no_permission)
+		if (!args.length) return warnUser(message, strings.command.args.none_given)
+
+		const member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
+		const reason = args.slice(1).join(' ') || 'Not Specified'
+
+		if (!member) return warnUser(message, strings.command.bean.specify_user)
+		if (member.id === message.author.id) return warnUser(message, strings.command.bean.cant_bean_self)
+		if (member.id === client.user.id) return await message.channel.send({ content: strings.command.no_i_dont_think_i_will })
+
+		else {
+			const embed = new MessageEmbed()
+				.setAuthor(message.author.tag, message.author.displayAvatarURL())
+				.setDescription(`Beaned ${member} \nReason: ${reason}`)
+				.setColor(settings.colors.blue)
+				.setTimestamp()
+			await message.reply({ embeds: [embed] })
+		}
+	}
+}
