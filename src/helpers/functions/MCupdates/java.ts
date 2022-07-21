5f40e769480b567bf6256515c1eadb505912a436
Robert <contact.e3nk6@aleeas.com>
2022-07-21 20:22:50 +0200

    simplify mc update checking due to mojang blocking requests to minecraft.net

--- src/helpers/functions/MCupdates/java.ts
+++ src/helpers/functions/MCupdates/java.ts
@@ -22,11 +22,11 @@ export async function loadJavaVersions() {
 
 	versions.versions.forEach((version) => {
 		// uncomment to test for specific versions being released
-		//if (version.id === "22w16b") return;
+		//if (version.id === "1.19.1-pre4") return;
 		minecraftVersionsCache.push(version.id);
 	});
 
-	console.log(`${info}Loaded ${minecraftVersionsCache.length} Minecraft Java versions`);
+	console.log(`${info}Loaded ${minecraftVersionsCache.length} Minecraft Java versions`); update checking due to mojang blocking requests to minecraft.net)
 }
 
 export async function updateJavaVersions(client: Client) {
@@ -49,62 +49,32 @@ export async function updateJavaVersions(client: Client) {
 			let updateChannel = (await client.channels.cache.get("773983707299184703")) as TextChannel;
 			let description;
 
-			// TODO: Find a way to reliably fetch the article from https://www.minecraft.net/content/minecraft-net/_jcr_content.articles.grid
-			if (version.id.includes("pre")) {
-				var pre = version.id.split("pre")[1];
-				let res;
-				res = await axios.get(
-					`https://www.minecraft.net/en-us/article/minecraft-${version.id
-						.split("-")[0]
-						.replace(/\./g, "-")}-pre-release-${pre}`,
-					{ validateStatus: () => true },
-				);
+      // TODO: Find a way to reliably fetch the article from https://www.minecraft.net/content/minecraft-net/_jcr_content.articles.grid
+      if (version.id.includes('pre')) {
+        const pre = version.id.split('pre')[1];
+        description = `A new pre-release of Minecraft Java was just released: \`${version.id}\`\nhttps://www.minecraft.net/en-us/article/minecraft-${version.id.split('-')[0].replace(/\./g, '-')}-pre-release-${pre}`;
+        if (pre !== '1') description = `A new pre-release of Minecraft Java was just released: \`${version.id}\`\nhttps://www.minecraft.net/en-us/article/minecraft-${version.id.split('-')[0].replace(/\./g, '-')}-pre-release-${pre}\nIf the above link doesn't work, try \`https://www.minecraft.net/en-us/article/minecraft-${version.id.split('-')[0].replace(/\./g, '-')}-pre-release-${pre - 1}\``;
+      } else if (version.id.includes('rc')) {
+        // it is very unlikely that there is more than one release candidate, so we'll just use the first one
+        // if there is a second one, Mojang will use the first article anyway
+        description = `A new release candidate of Minecraft Java was just released: \`${
+          version.id
+        }\`\nhttps://www.minecraft.net/en-us/article/minecraft-${version.id
+          .split('-')[0]
+          .replace(/\./g, '-')}-release-candidate-1`;
+      } else if (version.type === 'snapshot') {
+        description = `A new ${version.type} of Minecraft Java was just released: \`${
+          version.id
+        }\`\nhttps://www.minecraft.net/en-us/article/minecraft-snapshot-${version.id.slice(0, -1)}a`;
+      } else if (version.type === 'release') {
+        description = `A new ${version.type} of Minecraft Java was just released: \`${
+          version.id
+        }\`\nhttps://www.minecraft.net/en-us/article/minecraft-java-edition-${version.id.replace(/\./g, '-')}`;
+      }
 
-				if (res.status == 404) {
-					for (pre; pre > 1; pre--) {
-						res = await axios.get(
-							`https://www.minecraft.net/en-us/article/minecraft-${version.id
-								.split("-")[0]
-								.replace(/\./g, "-")}-pre-release-${pre}`,
-							{ validateStatus: () => true },
-						);
-
-						if (res.status !== 404) {
-							description = `A new pre-release of Minecraft Java was just released: \`${
-								version.id
-							}\`\nhttps://www.minecraft.net/en-us/article/minecraft-${version.id
-								.split("-")[0]
-								.replace(/\./g, "-")}-pre-release-${pre}`;
-							break;
-						}
-					}
-				} else {
-					description = `A new pre-release of Minecraft Java was just released: \`${
-						version.id
-					}\`\nhttps://www.minecraft.net/en-us/article/minecraft-${version.id
-						.split("-")[0]
-						.replace(/\./g, "-")}-pre-release-${pre}`;
-				}
-			} else if (version.id.includes("rc")) {
-				// it is very unlikely that there is more than one release candidate, so we'll just use the first one
-				// if there is a second one, Mojang will use the first article anyway
-				description = `A new release candidate of Minecraft Java was just released: \`${
-					version.id
-				}\`\nhttps://www.minecraft.net/en-us/article/minecraft-${version.id
-					.split("-")[0]
-					.replace(/\./g, "-")}-release-candidate-1`;
-			} else {
-				if (version.type === "snapshot")
-					description = `A new ${version.type} of Minecraft Java was just released: \`${
-						version.id
-					}\`\nhttps://www.minecraft.net/en-us/article/minecraft-snapshot-${version.id.slice(0, -1)}a`;
-				else if (version.type === "release")
-					description = `A new ${version.type} of Minecraft Java was just released: \`${
-						version.id
-					}\`\nhttps://www.minecraft.net/en-us/article/minecraft-java-edition-${version.id.replace(/\./g, "-")}`;
-			}
-
-			await updateChannel.send({ content: description });
-		}
-	});
+      await updateChannel.send({
+        content: description,
+      });
+    }
+  });
 }
