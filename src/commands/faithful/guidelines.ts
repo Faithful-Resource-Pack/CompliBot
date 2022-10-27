e489836a0ac8f6525eb9b61f5d6b194aa2f42b3a
Evorp <3vorpgaming@gmail.com>
2022-10-27 16:39:31 -0700

    Revert "more fixes if this doesn't work idk"
    This reverts commit 755254ca9c67f74d472d5d5b1c252e61ff0b8502.

--- src/commands/faithful/guidelines.ts
+++ src/commands/faithful/guidelines.ts
@@ -6,26 +6,10 @@ export const command: SlashCommand = {
 	servers: ["faithful", "faithful_extra", "classic_faithful"],
 	data: new SlashCommandBuilder()
 		.setName("guidelines")
-		.setDescription("Shows various Faithful texturing guidelines.")
-		.addStringOption((option) =>
-			option
-				.setName("pack")
-				.setDescription("The guidelines you want to view")
-				.addChoices(
-					{ name: "Faithful 32x", value: "faithful_32x" },
-					{ name: "Classic Faithful 32x", value: "classic_faithful_32x" },
-				)
-				.setRequired(true),
-		),
+		.setDescription("Shows the guidelines for the Faithful Resource Pack."),
 	execute: async (interaction: CommandInteraction) => {
-		let contents: string;
-		const variable = interaction.options.getString("name", false)
-		if (variable === "faithful_32x") {
-			contents = "https://docs.faithfulpack.net/pages/textures/texturing-guidelines";
-		}
-		else if (variable === "classic_faithful_32x") {
-			contents = "https://docs.faithfulpack.net/pages/classicfaithful/32x-texturing-guidelines";
-		};
-		interaction.reply({ content: contents, fetchReply: true }).then((message: Message) => message.deleteButton());
+		interaction
+			.reply({ content: "https://docs.faithfulpack.net/pages/textures/texturing-guidelines", fetchReply: true })
+			.then((message: Message) => message.deleteButton());
 	},
 };
