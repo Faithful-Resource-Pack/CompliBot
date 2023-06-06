21ff1e397007933a970ad60723eeb7c2ced6e281
Evorp <3vorpgaming@gmail.com>
2023-06-05 18:26:51 -0700

    finish #232 (submit multiple textures in one message)
    it copies over co-authors, works across editions, pastes descriptions, the whole nine yards

--- functions/textures/submission/submitTexture.js
+++ functions/textures/submission/submitTexture.js
@@ -8,25 +8,6 @@ const { MessageEmbed, MessageAttachment } = require('discord.js');
 const { Permissions } = require('discord.js');
 const { magnifyAttachment } = require('../magnify')
 
-const MinecraftSorter = (a, b) => {
-  const aSplit = a.split('.').map(s => parseInt(s))
-  const bSplit = b.split('.').map(s => parseInt(s))
-
-  const upper = Math.min(aSplit.length, bSplit.length)
-  let i = 0
-  let result = 0
-  while (i < upper && result == 0) {
-    result = (aSplit[i] == bSplit[i]) ? 0 : (aSplit[i] < bSplit[i] ? -1 : 1) // each number
-    ++i
-  }
-
-  if (result != 0) return result
-
-  result = (aSplit.length == bSplit.length) ? 0 : (aSplit.length < bSplit.length ? -1 : 1) // longer length wins
-
-  return result
-}
-
 /**
  * Check if the given texture exist, and embed it if true
  * @author Juknum
@@ -34,131 +15,131 @@ const MinecraftSorter = (a, b) => {
  * @param {DiscordMessage} message
  */
 async function submitTexture(client, message) {
+  // break if no file is attached
+  if (message.attachments.size == 0) return invalidSubmission(message, strings.command.push.image_not_attached)
   let args = message.content.split(' ')
 
-  // get the texture ID
-  let id = args.filter(el => el.startsWith('(#') && el.endsWith(')') && !isNaN(el.slice(2).slice(0, -1))).map(el => el.slice(2).slice(0, -1))
-  id = id[0]
-  // get the name
-  let search = args.filter(el => el.startsWith('(') && el.endsWith(')')).map(el => el.slice(1).slice(0, -1))
-  search = search[0]
-  // get the description
-  let description = args.join(' ').replace(`(${search})`, '').replace(`[#${id}]`, '')
+  // not entirely sure why the first arg exists but it seems to fix problems I had with iterating over maps
+  for (let [_, attachment] of message.attachments) {
+    // get the texture ID
+    let id = args.filter(el => el.startsWith('(#') && el.endsWith(')') && !isNaN(el.slice(2).slice(0, -1))).map(el => el.slice(2).slice(0, -1))
+    id = id[0]
+    // get the description
+    let description = args.join(' ').replace(`(${search})`, '').replace(`[#${id}]`, '')
 
-  // parameters for the embed
-  let param = {
-    description: description,
-  }
-
-  // same if no file is attached
-  if (message.attachments.size == 0) return invalidSubmission(message, strings.command.push.image_not_attached)
-  // same if it's not a PNG
-  if (
-    message.attachments.first().url.endsWith('.zip') ||
-    message.attachments.first().url.endsWith('.rar') ||
-    message.attachments.first().url.endsWith('.7zip')
-  ) return invalidSubmission(message, strings.command.push.invalid_format)
-
-  // if no name are given, take the image url and get it's name
-  if (!search) search = message.attachments.first().url.split('/').slice(-1)[0].replace('.png', '')
-
-  // detect co-authors as mentions:
-  let mentions = message.mentions.users
-  param.authors = [message.author.id]
-  mentions.forEach(mention => { if (!param.authors.includes(mention.id)) param.authors.push(mention.id) })
-
-  let results = new Array()
-
-  // priority to ids -> faster
-  if (id) {
-    let texture = await textures.get(id).catch(err => invalidSubmission(message, strings.command.push.unknown_id + err))
-    await makeEmbed(client, message, texture, param)
-  }
-  // no id given, search texture
-  else if (!id && search) {
-    /*var waitEmbed = new Discord.MessageEmbed()
-      .setTitle('Loading')
-      .setDescription(string('command.texture.searching'))
-      .setColor(settings.colors.blue)
-    const waitEmbedMessage = await message.reply({embeds: [waitEmbed]});*/
-
-    // partial texture name (_sword, _axe -> diamond_sword, diamond_axe...)
-    if (search.startsWith('_') || search.endsWith('_')) {
-      results = await textures.search([{
-        field: "name",
-        criteria: "includes",
-        value: search
-      }])
+    // parameters for the embed
+    let param = {
+      description: description,
     }
-    // looking for path + texture (block/stone -> stone)
-    else if (search.startsWith('/') || search.endsWith('/')) {
-      results = await paths.search([{
-        field: "path",
-        criteria: "includes",
-        value: search
-      }])
-      // transform paths results into textures
-      let output = new Array()
-      for (let i = 0; results[i]; i++) {
-        let use = await results[i].use()
-        output.push(await textures.get(use.textureID))
-      }
-      results = output
+    // same if it's not a PNG
+    if (!attachment.url.endsWith('.png')) {
+      invalidSubmission(message, strings.command.push.invalid_format);
+      continue;
     }
-    // looking for all exact matches (stone -> stone.png)
-    else {
-      results = await textures.search([{
-        field: "name",
-        criteria: "==",
-        value: search
-      }])
 
-      if (results.length == 0) {
-        // no equal result, searching with includes
+    // take image url to get name of texture
+    let search = attachment.url.split('/').slice(-1)[0].replace('.png', '')
+
+    // detect co-authors as mentions:
+    let mentions = message.mentions.users
+    param.authors = [message.author.id]
+    mentions.forEach(mention => { if (!param.authors.includes(mention.id)) param.authors.push(mention.id) })
+
+    let results = new Array()
+
+    // priority to ids -> faster
+    if (id) {
+      let texture = await textures.get(id).catch(err => invalidSubmission(message, strings.command.push.unknown_id + err))
+      await makeEmbed(client, message, texture, attachment, param)
+    }
+    // no id given, search texture
+    else if (!id && search) {
+      /*var waitEmbed = new Discord.MessageEmbed()
+        .setTitle('Loading')
+        .setDescription(string('command.texture.searching'))
+        .setColor(settings.colors.blue)
+      const waitEmbedMessage = await message.reply({embeds: [waitEmbed]});*/
+
+      // partial texture name (_sword, _axe -> diamond_sword, diamond_axe...)
+      if (search.startsWith('_') || search.endsWith('_')) {
         results = await textures.search([{
           field: "name",
-          criteria: 'includes',
+          criteria: "includes",
           value: search
         }])
       }
-    }
+      // looking for path + texture (block/stone -> stone)
+      else if (search.startsWith('/') || search.endsWith('/')) {
+        results = await paths.search([{
+          field: "path",
+          criteria: "includes",
+          value: search
+        }])
+        // transform paths results into textures
+        let output = new Array()
+        for (let i = 0; results[i]; i++) {
+          let use = await results[i].use()
+          output.push(await textures.get(use.textureID))
+        }
+        results = output
+      }
+      // looking for all exact matches (stone -> stone.png)
+      else {
+        results = await textures.search([{
+          field: "name",
+          criteria: "==",
+          value: search
+        }])
 
-    if (results.length > 1) {
-      let choice = [];
-      for (let i = 0; results[i]; i++) {
-        let uses = await results[i].uses()
-        let paths = await uses[0].paths()
-
-        choice.push(`\`[#${results[i].id}]\` ${results[i].name.replace(search, `**${search}**`).replace(/_/g, '\\_')} — ${paths[0].path.replace(search, `**${search}**`).replace(/_/g, '\\_')}`)
+        if (results.length == 0) {
+          // no equal result, searching with includes
+          results = await textures.search([{
+            field: "name",
+            criteria: 'includes',
+            value: search
+          }])
+        }
       }
 
-      //if (!waitEmbedMessage.deleted) await waitEmbedMessage.delete();
-      choiceEmbed(message, {
-        title: `${results.length} results, react to choose one!`,
-        description: strings.command.texture.search_description,
-        footer: `${message.client.user.username}`,
-        propositions: choice
-      })
-        .then(choice => {
-          return makeEmbed(client, message, results[choice.index], param)
+      if (results.length > 1) {
+        let choice = [];
+        for (let i = 0; results[i]; i++) {
+          let uses = await results[i].uses()
+          let paths = await uses[0].paths()
+
+          choice.push(`\`[#${results[i].id}]\` ${results[i].name.replace(search, `**${search}**`).replace(/_/g, '\\_')} — ${paths[0].path.replace(search, `**${search}**`).replace(/_/g, '\\_')}`)
+        }
+
+        //if (!waitEmbedMessage.deleted) await waitEmbedMessage.delete();
+        choiceEmbed(message, {
+          title: `${results.length} results, react to choose one!`,
+          description: strings.command.texture.search_description,
+          footer: `${message.client.user.username}`,
+          propositions: choice
         })
-        .catch((message, error) => {
-          if (process.env.DEBUG) console.error(message, error)
-        })
-    }
-    else if (results.length == 1) {
-      await makeEmbed(client, message, results[0], param)
-      //if (!waitEmbedMessage.deleted) await waitEmbedMessage.delete()
-    }
-    else {
-      //if (!waitEmbedMessage.deleted) await waitEmbedMessage.delete()
-      await invalidSubmission(message, strings.command.texture.does_not_exist + '\n' + search)
+          .then(choice => {
+            makeEmbed(client, message, results[choice.index], attachment, param)
+          })
+          .catch((message, error) => {
+            if (process.env.DEBUG) console.error(message, error)
+          })
+          continue;
+      }
+      else if (results.length == 1) {
+        await makeEmbed(client, message, results[0], attachment, param)
+        //if (!waitEmbedMessage.deleted) await waitEmbedMessage.delete()
+      }
+      else {
+        //if (!waitEmbedMessage.deleted) await waitEmbedMessage.delete()
+        await invalidSubmission(message, strings.command.texture.does_not_exist + '\n' + search)
+      }
     }
   }
+  if (!message.deleted) setTimeout(() => message.delete(), 10);
 }
 
 const EMOJIS = [settings.emojis.upvote, settings.emojis.downvote, settings.emojis.see_more]
-async function makeEmbed(client, message, texture, param = new Object()) {
+async function makeEmbed(client, message, texture, attachment, param = new Object()) {
 
   /** @type {import("../../helpers/firestorm/texture_use.js").TextureUse[]} */
   let uses = await texture.uses()
@@ -191,9 +172,9 @@ async function makeEmbed(client, message, texture, param = new Object()) {
     )
 
   const attachmentThumbnail = await magnifyAttachment(`${settings.repositories.raw.default[thumbnail.edition.toLowerCase()]}${thumbnail.version}/${thumbnail.path}`)
-  const attachment = new MessageAttachment(message.attachments.first().url, texture.name + '.png')
+  const attachmentMain = new MessageAttachment(attachment.url, texture.name + '.png')
 
-  const imgMessage = await client.channels.cache.get('916766396170518608').send({files: [attachmentThumbnail, attachment]})
+  const imgMessage = await client.channels.cache.get('916766396170518608').send({files: [attachmentThumbnail, attachmentMain]})
 
   var imgArray = new Array()
 
@@ -211,7 +192,6 @@ async function makeEmbed(client, message, texture, param = new Object()) {
 
   // send the embed
   const msg = await message.channel.send({ embeds: [embed] });
-  if (!message.deleted) setTimeout(() => message.delete(), 10);
 
   // add reactions to the embed
   for (const emojiID of EMOJIS) {
@@ -238,6 +218,25 @@ async function invalidSubmission(message, error = 'Not given') {
   } catch (error) {
     console.error(error)
   }
+}
+
+const MinecraftSorter = (a, b) => {
+  const aSplit = a.split('.').map(s => parseInt(s))
+  const bSplit = b.split('.').map(s => parseInt(s))
+
+  const upper = Math.min(aSplit.length, bSplit.length)
+  let i = 0
+  let result = 0
+  while (i < upper && result == 0) {
+    result = (aSplit[i] == bSplit[i]) ? 0 : (aSplit[i] < bSplit[i] ? -1 : 1) // each number
+    ++i
+  }
+
+  if (result != 0) return result
+
+  result = (aSplit.length == bSplit.length) ? 0 : (aSplit.length < bSplit.length ? -1 : 1) // longer length wins
+
+  return result
 }
 
 exports.submitTexture = submitTexture
