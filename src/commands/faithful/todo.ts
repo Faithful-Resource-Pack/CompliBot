342792641a1277387c8001b34217d89fcebb9bd3
Evorp <3vorpgaming@gmail.com>
2023-06-11 17:04:44 -0700

    remove todo and replace with basic link

---
+++ src/commands/faithful/todo.ts
@@ -0,0 +1,15 @@
+import { SlashCommand } from "@interfaces";
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandInteraction, Message } from "@client";
+
+export const command: SlashCommand = {
+	servers: ["faithful", "faithful_extra", "classic_faithful"],
+	data: new SlashCommandBuilder()
+		.setName("todo")
+		.setDescription("Shows the to-do list for Faithful 32x."),
+	execute: async (interaction: CommandInteraction) => {
+		interaction
+			.reply({ content: "https://docs.google.com/document/d/1OXGHKiYJej0qvNgZWfFuYauyL1OjpL31usgK4dwuIkI/", fetchReply: true })
+			.then((message: Message) => message.deleteButton());
+	},
+};
