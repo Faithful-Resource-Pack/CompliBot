940c12e7b8a5fd851c500e76db7a9f69c0a5a5b3
Evorp <3vorpgaming@gmail.com>
2023-06-15 21:18:11 -0700

    view raw texture

--- functions/textures/submission/editSubmission.js
+++ functions/textures/submission/editSubmission.js
@@ -1,11 +1,11 @@
 const settings = require('../../../resources/settings.json')
 
 const { Permissions } = require('discord.js');
-const { magnify } = require('../../../functions/textures/magnify')
-const { palette } = require('../../../functions/textures/palette')
+const { magnify } = require('../magnify')
+const { palette } = require('../palette')
 const { tile } = require('../tile')
+const { viewRaw } = require('../viewRaw')
 const { warnUser } = require('../../../helpers/warnUser')
-const compareFunction = require('../compare')
 const { downloadResults } = require("../admission/downloadResults")
 const { changeStatus } = require('./changeStatus')
 
@@ -28,91 +28,93 @@ async function editSubmission(client, reaction, user) {
 
   const authorID = await message.embeds[0].fields[0].value.split('\n').map(el => el.replace('<@', '').replace('!', '').replace('>', ''))[0]
 
-  if (reaction.emoji.id === settings.emojis.see_more || reaction.emoji.id === settings.emojis.see_more_old) {
+  if (reaction.emoji.id !== settings.emojis.see_more && reaction.emoji.id !== settings.emojis.see_more_old) return
 
-    reaction.remove().catch(err => { if (process.DEBUG) console.error(err) })
+  reaction.remove().catch(err => { if (process.DEBUG) console.error(err) })
 
-    let EMOJIS = [settings.emojis.see_less, settings.emojis.delete, settings.emojis.instapass, settings.emojis.invalid, settings.emojis.magnify, settings.emojis.palette, settings.emojis.tile, settings.emojis.compare]
+  let EMOJIS = [
+    settings.emojis.see_less,
+    settings.emojis.delete,
+    settings.emojis.instapass,
+    settings.emojis.invalid,
+    settings.emojis.magnify,
+    settings.emojis.palette,
+    settings.emojis.tile,
+    settings.emojis.view_raw
+  ]
 
-    // if the message does not have up/down vote react, remove INSTAPASS & INVALID from the emojis list (already instapassed or votes flushed)
-    if (!message.embeds[0].fields[1].value.includes(settings.emojis.pending) && !message.embeds[0].fields[1].value.includes('⏳'))
-      EMOJIS = EMOJIS.filter(emoji => emoji !== settings.emojis.instapass && emoji !== settings.emojis.invalid && emoji !== settings.emojis.delete)
+  // if the message does not have up/down vote react, remove INSTAPASS & INVALID from the emojis list (already instapassed or votes flushed)
+  if (!message.embeds[0].fields[1].value.includes(settings.emojis.pending))
+    EMOJIS = EMOJIS.filter(emoji => emoji !== settings.emojis.instapass && emoji !== settings.emojis.invalid && emoji !== settings.emojis.delete)
 
-    // if the message is in #council-vote remove delete reaction (avoid misclick)
-    const councilChannels = Object.values(settings.submission.packs).map(i => i.channels.council);
+  // if the message is in #council-vote remove delete reaction (avoid misclick)
+  const councilChannels = Object.values(settings.submission.packs).map(i => i.channels.council);
 
-    if (councilChannels.includes(message.channel.id)) {
-      EMOJIS = EMOJIS.filter(emoji => emoji !== settings.emojis.delete)
-    }
+  if (councilChannels.includes(message.channel.id)) {
+    EMOJIS = EMOJIS.filter(emoji => emoji !== settings.emojis.delete)
+  }
 
-    // add reacts
-    for (let i = 0; EMOJIS[i]; i++) await message.react(EMOJIS[i])
+  // add reacts
+  for (let emoji of EMOJIS) await message.react(emoji)
 
-    // make the filter
-    const filter = (REACT, USER) => {
-      return EMOJIS.includes(REACT.emoji.id) && USER.id === user.id
-    }
+  // make the filter
+  const filter = (REACT, USER) => {
+    return EMOJIS.includes(REACT.emoji.id) && USER.id === user.id
+  }
 
-    // await reaction from the user
-    message.awaitReactions({ filter, max: 1, time: 30000, errors: ['time'] })
-      .then(async collected => {
-        const REACTION = collected.first()
-        const USER_ID = [...collected.first().users.cache.values()].filter(user => user.bot === false).map(user => user.id)[0]
+  // await reaction from the user
+  message.awaitReactions({ filter, max: 1, time: 30000, errors: ['time'] })
+    .then(async collected => {
+      const REACTION = collected.first()
+      const USER_ID = [...collected.first().users.cache.values()].filter(user => user.bot === false).map(user => user.id)[0]
 
-        if (REACTION.emoji.id === settings.emojis.palette) palette(message, message.embeds[0].thumbnail.url, user.id)
-        else if (REACTION.emoji.id === settings.emojis.magnify) magnify(message, message.embeds[0].thumbnail.url, user.id)
-        else if (REACTION.emoji.id === settings.emojis.tile) tile(message, message.embeds[0].thumbnail.url, 'grid', user.id)
+      switch (REACTION.emoji.id) {
+        case settings.emojis.magnify:
+          magnify(message, message.embeds[0].thumbnail.url, user.id);
+          break;
+        case settings.emojis.palette:
+          palette(message, message.embeds[0].thumbnail.url, user.id);
+          break;
+        case settings.emojis.tile:
+          tile(message, message.embeds[0].thumbnail.url, 'grid', user.id);
+          break;
+        case settings.emojis.view_raw:
+          viewRaw(message, message.embeds[0].thumbnail.url, user.id);
+          break;
+      }
 
-        /**
-         * TODO: find why you can't have 2 textures of the same resolution in the drawer.urls (the texture isn't processed??)
-         */
-        else if (REACTION.emoji.id === settings.emojis.compare) {
-          /** @type {MessageEmbed} */
-          const embed = message.embeds[0]
-
-          const currentSubmissionUrl = embed.thumbnail.url
-          const textureTitle = embed.title
-          const textureId = textureTitle.substring(textureTitle.indexOf('#') + 1, textureTitle.indexOf(']')).trim()
-
-          /** @type {import('../compare').CompareOption} */
-          const options = {
-            id: textureId,
-            user: user,
-            images: [currentSubmissionUrl]
-          }
-
-          await compareFunction(options)
+      if (
+        member.permissions.has(Permissions.FLAGS.ADMINISTRATOR) ||
+        member.roles.cache.some(role => role.name.toLowerCase().includes("council"))
+      ) {
+        if (REACTION.emoji.id === settings.emojis.instapass) {
+          removeReact(message, [settings.emojis.upvote, settings.emojis.downvote])
+          changeStatus(message, `<:instapass:${settings.emojis.instapass}> Instapassed by <@${member.id}>`, settings.colors.yellow)
+          instapass(client, message)
+        } else if (REACTION.emoji.id === settings.emojis.invalid) {
+          removeReact(message, [settings.emojis.upvote, settings.emojis.downvote])
+          changeStatus(message, `<:invalid:${settings.emojis.invalid}> Invalidated by <@${member.id}>`, settings.colors.red)
         }
-        if (
-          member.permissions.has(Permissions.FLAGS.ADMINISTRATOR) ||
-          member.roles.cache.some(role => role.name.toLowerCase().includes("council"))
-        ) {
-          if (REACTION.emoji.id === settings.emojis.instapass) {
-            removeReact(message, [settings.emojis.upvote, settings.emojis.downvote])
-            changeStatus(message, `<:instapass:${settings.emojis.instapass}> Instapassed by <@${member.id}>`, settings.colors.yellow)
-            instapass(client, message)
-          } else if (REACTION.emoji.id === settings.emojis.invalid) {
-            removeReact(message, [settings.emojis.upvote, settings.emojis.downvote])
-            changeStatus(message, `<:invalid:${settings.emojis.invalid}> Invalidated by <@${member.id}>`, settings.colors.red)
-          }
-        }
+      }
 
-        // delete message only if the first author of the field 0 is the discord user who reacted, or if the user who react is admin
-        if (REACTION.emoji.id === settings.emojis.delete && (USER_ID === authorID || member.permissions.has(Permissions.FLAGS.ADMINISTRATOR))) return await message.delete()
+      // delete message only if the first author of the field 0 is the discord user who reacted, or if the user who react is admin
+      if (
+        REACTION.emoji.id === settings.emojis.delete &&
+        (USER_ID === authorID || member.permissions.has(Permissions.FLAGS.ADMINISTRATOR))
+      ) return await message.delete()
 
+      removeReact(message, EMOJIS)
+      await message.react(client.emojis.cache.get(settings.emojis.see_more))
+
+    })
+    .catch(async err => {
+      if (!message.deleted) {
         removeReact(message, EMOJIS)
         await message.react(client.emojis.cache.get(settings.emojis.see_more))
+      }
 
-      })
-      .catch(async err => {
-        if (!message.deleted) {
-          removeReact(message, EMOJIS)
-          await message.react(client.emojis.cache.get(settings.emojis.see_more))
-        }
-
-        console.log(err)
-      })
-  }
+      console.log(err)
+    })
 }
 
 async function instapass(client, message) {
