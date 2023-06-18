d6c32790872550028a37445e599ba8d30d9e7caf
Evorp <3vorpgaming@gmail.com>
2023-06-17 21:14:38 -0700

    remove unnecessary operations

--- functions/textures/sendAttachment.js
+++ functions/textures/sendAttachment.js
@@ -10,25 +10,23 @@ const { warnUser } = require('../../helpers/warnUser');
  * @returns the sent message
  */
 async function sendAttachment(message, attachment, userID, embed = null) {
-    let channel;
-    for (let [key, value] of Object.entries(settings.guilds)) {
-        if (message.guild.id == value) {
-            // since the bot command and guild key names are the same
-            channel = message.guild.channels.cache.get(settings.channels.bot_commands[key]);
-            break;
-        }
-    }
-
     let embedMessage;
-
     try { // try to send in DMs
         const member = await message.guild.members.cache.get(userID);
         if (embed) embedMessage = await member.send({ embeds: [embed], files: [attachment] });
         else embedMessage = await member.send({ files: [attachment] });
     } catch { // user has DMs disabled
-        // no valid #bot-commands was found, unnecessary info if using DMs so I put it here
+        let channel;
+        for (let [guildName, guildID] of Object.entries(settings.guilds)) {
+            if (message.guild.id == guildID) {
+                // since the bot command and guild key names are the same
+                channel = message.guild.channels.cache.get(settings.channels.bot_commands[guildName]);
+                break;
+            }
+        }
         if (!channel) return warnUser(message, `You aren't in a valid server!`);
-        embedMessage = await channel.send({ content: `<@!${userID}>`, embeds: [embed], files: [attachment] });
+        if (embed) embedMessage = await channel.send({ content: `<@!${userID}>`, embeds: [embed], files: [attachment] });
+        else embedMessage = await channel.send({ content: `<@!${userID}>`, files: [attachment] });
     }
 
     addDeleteReact(embedMessage, message, true);
