ba1effdbce035aaabb53e21e839a27107a7b4c02
Evorp <3vorpgaming@gmail.com>
2023-06-21 16:30:29 -0700

    no

--- src/commands/fun/order.ts
+++ src/commands/fun/order.ts
@@ -14,7 +14,7 @@ const options: { name: string; value: string }[] = [
 	{ name: "pizza", value: "https://i0.wp.com/metro.co.uk/wp-content/uploads/2016/02/pizza-cheese.gif" },
 	{ name: "soup", value: "https://c.tenor.com/45SSoTETymIAAAAS/sopita-de-fideo-noodle.gif" },
 	{ name: "burger", value: "https://c.tenor.com/tdFqDJemKpUAAAAC/mcdonalds-big-mac.gif" },
-	{ name: "poop", value: "https://c.tenor.com/-Rv2hPlRKA0AAAAC/i-see-what-you-did-there-steve-carell.gif" },
+	{ name: "poop", value: "https://media.tenor.com/QA6mPKs100UAAAAC/caught-in.gif" },
 	{ name: "66", value: "https://media1.tenor.com/images/fb7250a2ef993a37e9c7f48af760821c/tenor.gif" },
 	{ name: "help", value: "https://c.tenor.com/yi5btxWVAwwAAAAS/help-shouting.gif" },
 	{ name: "ice", value: "https://c.tenor.com/ySPd8qwdV7QAAAAC/frozen-ice.gif" },
