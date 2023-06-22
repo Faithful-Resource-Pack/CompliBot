bfbe0aa768d52ae9ad24910ae4ba2ad07511cba7
Evorp <3vorpgaming@gmail.com>
2023-06-21 19:20:49 -0700

    Revert "more async await"
    This reverts commit 51b980f6e4c01b15c26726af1e6aa0abc3f09f47.

--- functions/textures/animate.js
+++ functions/textures/animate.js
@@ -21,83 +21,116 @@ async function animate(message, valMCMETA, valURL) {
 
 	let texture = []
 
-	const dimension = await getMeta(valURL)
-	if (dimension.width == dimension.height) return warnUser(message, strings.command.image.cant_animate)
-	if (dimension.width * FACTOR > 4096) return warnUser(message, strings.command.image.too_wide)
-	if (dimension.width * FACTOR > 1024) FACTOR = 1
+	getMeta(valURL).then(async (dimension) => {
+		if (dimension.width == dimension.height) return warnUser(message, strings.command.image.cant_animate)
+		if (dimension.width * FACTOR > 4096) return warnUser(message, strings.command.image.too_wide)
+		if (dimension.width * FACTOR > 1024) FACTOR = 1
 
-	texture.canvas = await sizeUP(valURL, dimension)
-	texture.width = dimension.width * FACTOR
-	texture.height = dimension.height * FACTOR
+		texture.canvas = await sizeUP(valURL, dimension)
+		texture.width = dimension.width * FACTOR
+		texture.height = dimension.height * FACTOR
 
-	// NOTE: Width & Height properties from MCMETA aren't supported
+		// NOTE: Width & Height properties from MCMETA aren't supported
 
-	let canvas = Canvas.createCanvas(texture.width, texture.width)
-	let context = canvas.getContext('2d')
+		let canvas = Canvas.createCanvas(texture.width, texture.width)
+		let context = canvas.getContext('2d')
 
-	let MCMETA = typeof valMCMETA === 'object' ? valMCMETA : { animation: {} }
-	if (!MCMETA.animation) MCMETA.animation = {}
+		let MCMETA = typeof valMCMETA === 'object' ? valMCMETA : { animation: {} }
+		if (!MCMETA.animation) MCMETA.animation = {}
 
-	// Initialization:
-	let frametime = MCMETA.animation.frametime || 1
-	let frames = []
+		// Initialization:
+		let frametime = MCMETA.animation.frametime || 1
+		let frames = []
 
-	// MCMETA.animation.frames is defined
-	if (Array.isArray(MCMETA.animation.frames) && MCMETA.animation.frames.length > 0) {
-		for (let i = 0; i < MCMETA.animation.frames.length; i++) {
-			const frame = MCMETA.animation.frames[i]
+		// MCMETA.animation.frames is defined
+		if (Array.isArray(MCMETA.animation.frames) && MCMETA.animation.frames.length > 0) {
+			for (let i = 0; i < MCMETA.animation.frames.length; i++) {
+				const frame = MCMETA.animation.frames[i]
 
-			if (typeof frame === 'number') {
-				frames.push({
-					index: frame,
-					duration: frametime
-				})
+				if (typeof frame === 'number') {
+					frames.push({
+						index: frame,
+						duration: frametime
+					})
+				}
+				else if (typeof frame === 'object') {
+					frames.push({
+						index: frame.index || i,
+						duration: frame.time || frametime
+					})
+				}
+				// If wrong frames support is given
+				else {
+					frames.push({
+						index: i,
+						duration: frametime
+					})
+				}
 			}
-			else if (typeof frame === 'object') {
-				frames.push({
-					index: frame.index || i,
-					duration: frame.time || frametime
-				})
-			}
-			// If wrong frames support is given
-			else {
+		}
+		// MCMETA.animation.frames is not defined
+		else {
+			for (let i = 0; i < texture.height / texture.width; i++) {
 				frames.push({
 					index: i,
 					duration: frametime
 				})
 			}
 		}
-	}
-	// MCMETA.animation.frames is not defined
-	else {
-		for (let i = 0; i < texture.height / texture.width; i++) {
-			frames.push({
-				index: i,
-				duration: frametime
-			})
+
+		// Draw frames:
+		const encoder = new GIFEncoderFixed(texture.width, texture.width)
+		encoder.start()
+		encoder.setTransparent(true)
+
+		context.globalCompositeOperation = 'copy'
+
+		if (MCMETA.animation.interpolate) {
+			let ratio = 0
+
+			let limit = frametime
+			if (limit >= 100) limit /= 10
+
+			for (let i = 0; i < frames.length; i++) {
+				for (let y = 1; y <= limit; y++) {
+					context.clearRect(0, 0, canvas.width, canvas.height)
+					context.imageSmoothingEnabled = texture.width > canvas.width
+					context.globalAlpha = 1
+
+					// frame i (always 100%)
+					context.drawImage(
+						texture.canvas, 										// image
+						0, texture.width * frames[i].index, // sx, sy
+						texture.width, texture.width,			  // sWidth, sHeight
+						0, 0,															  // dx, dy
+						canvas.width, canvas.height					// dWidth, dHeight
+					)
+
+					ratio = (100 / frametime) * y
+					context.globalAlpha = ratio / 100
+
+					// frame i + 1 (follow the ratio)
+					context.drawImage(
+						texture.canvas,																						// image
+						0, texture.width * frames[(i + 1) % frames.length].index, // sx, sy
+						texture.width, texture.width,															// sWidth, sHeight
+						0, 0,																											// dx, dy
+						canvas.width, canvas.height																// dWidth, dHeight
+					)
+
+					if (limit == frametime) encoder.setDelay(50) // frames count == frametime -> time of 1 frame == 1 tick -> 50ms
+					else encoder.setDelay(500)
+					encoder.addFrame(context)
+				}
+			}
 		}
-	}
-
-	// Draw frames:
-	const encoder = new GIFEncoderFixed(texture.width, texture.width)
-	encoder.start()
-	encoder.setTransparent(true)
-
-	context.globalCompositeOperation = 'copy'
-
-	if (MCMETA.animation.interpolate) {
-		let ratio = 0
-
-		let limit = frametime
-		if (limit >= 100) limit /= 10
-
-		for (let i = 0; i < frames.length; i++) {
-			for (let y = 1; y <= limit; y++) {
+		else {
+			for (let i = 0; i < frames.length; i++) {
 				context.clearRect(0, 0, canvas.width, canvas.height)
 				context.imageSmoothingEnabled = texture.width > canvas.width
 				context.globalAlpha = 1
 
-				// frame i (always 100%)
+				// see: https://media.prod.mdn.mozit.cloud/attachments/2012/07/09/225/46ffb06174df7c077c89ff3055e6e524/Canvas_drawimage.jpg
 				context.drawImage(
 					texture.canvas, 										// image
 					0, texture.width * frames[i].index, // sx, sy
@@ -106,51 +139,19 @@ async function animate(message, valMCMETA, valURL) {
 					canvas.width, canvas.height					// dWidth, dHeight
 				)
 
-				ratio = (100 / frametime) * y
-				context.globalAlpha = ratio / 100
-
-				// frame i + 1 (follow the ratio)
-				context.drawImage(
-					texture.canvas,																						// image
-					0, texture.width * frames[(i + 1) % frames.length].index, // sx, sy
-					texture.width, texture.width,															// sWidth, sHeight
-					0, 0,																											// dx, dy
-					canvas.width, canvas.height																// dWidth, dHeight
-				)
-
-				if (limit == frametime) encoder.setDelay(50) // frames count == frametime -> time of 1 frame == 1 tick -> 50ms
-				else encoder.setDelay(500)
+				encoder.setDelay(50 * frames[i].duration)
 				encoder.addFrame(context)
 			}
 		}
-	}
-	else {
-		for (let i = 0; i < frames.length; i++) {
-			context.clearRect(0, 0, canvas.width, canvas.height)
-			context.imageSmoothingEnabled = texture.width > canvas.width
-			context.globalAlpha = 1
 
-			// see: https://media.prod.mdn.mozit.cloud/attachments/2012/07/09/225/46ffb06174df7c077c89ff3055e6e524/Canvas_drawimage.jpg
-			context.drawImage(
-				texture.canvas, 										// image
-				0, texture.width * frames[i].index, // sx, sy
-				texture.width, texture.width,			  // sWidth, sHeight
-				0, 0,															  // dx, dy
-				canvas.width, canvas.height					// dWidth, dHeight
-			)
+		encoder.finish()
 
-			encoder.setDelay(50 * frames[i].duration)
-			encoder.addFrame(context)
-		}
-	}
+		// Send result:
+		const attachment = new MessageAttachment(encoder.out.getData(), 'output.gif')
 
-	encoder.finish()
-
-	// Send result:
-	const attachment = new MessageAttachment(encoder.out.getData(), 'output.gif')
-
-	const embedMessage = await message.reply({ files: [attachment] })
-	addDeleteReact(embedMessage, message, true)
+		const embedMessage = await message.reply({ files: [attachment] })
+		addDeleteReact(embedMessage, message, true)
+	})
 }
 
 /**
