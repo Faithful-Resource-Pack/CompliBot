99eff0325d773b6af59e543e94ce7909f898e43e
Evorp <3vorpgaming@gmail.com>
2023-06-25 15:43:40 -0700

    add CF to-do to /todo

--- src/commands/faithful/todo.ts
+++ src/commands/faithful/todo.ts
@@ -5,10 +5,29 @@ import { CommandInteraction, Message } from "@client";
 export const command: SlashCommand = {
 	data: new SlashCommandBuilder()
 		.setName("todo")
-		.setDescription("Shows the to-do list for Faithful 32x."),
+		.setDescription("Shows various project to-do lists.")
+		.addStringOption((option) =>
+			option
+				.setName("pack")
+				.setDescription("The to-do list you want to view")
+				.addChoices(
+					{ name: "Faithful 32x", value: "faithful_32x" },
+					{ name: "Classic Faithful 32x Jappa", value: "classic_faithful_32x" },
+				)
+				.setRequired(true),
+		),
 	execute: async (interaction: CommandInteraction) => {
-		interaction
-			.reply({ content: "https://docs.google.com/document/d/1OXGHKiYJej0qvNgZWfFuYauyL1OjpL31usgK4dwuIkI/", fetchReply: true })
-			.then((message: Message) => message.deleteButton());
+		let contents: string;
+		const pack = interaction.options.getString("pack", false)
+		switch (pack) {
+			case "faithful_32x":
+				contents = "https://docs.google.com/document/d/1OXGHKiYJej0qvNgZWfFuYauyL1OjpL31usgK4dwuIkI/";
+				break;
+			case "classic_faithful_32x":
+				contents = "https://docs.google.com/document/d/1lw5EvJhVbubNPm3ZiOXAr8Aos9gCkmz-rh03uIMuqY0/";
+				break;
+		}
+
+		interaction.reply({ content: contents, fetchReply: true }).then((message: Message) => message.deleteButton());
 	},
 };
