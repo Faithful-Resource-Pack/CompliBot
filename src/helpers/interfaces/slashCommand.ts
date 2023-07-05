a8db7298dea1e76301b69fa130b88d1fa22d5a32
Evorp <3vorpgaming@gmail.com>
2023-07-04 20:55:57 -0700

    more permissions v1 stuff

--- src/helpers/interfaces/slashCommand.ts
+++ src/helpers/interfaces/slashCommand.ts
@@ -4,7 +4,6 @@ import { Client } from "@client";
 
 export interface SlashCommand {
 	servers?: Array<string>;
-	permissions?: Permissions | undefined;
 	data: SyncSlashCommandBuilder | AsyncSlashCommandBuilder;
 	execute: Collection<string, SlashCommandI> | SlashCommandI;
 }
