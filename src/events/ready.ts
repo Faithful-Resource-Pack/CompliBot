f5452c69c4fb936b31d94ac0b84df7421b55f13f
Robert Reichel <contact@reichelrobert.de>
2023-07-12 21:40:57 +0200

    Update ready.ts

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -5,7 +5,7 @@ export const event: Event = {
 	name: "ready",
 	run: async (client) => {
 		console.log(`${success}${client.user.username} is online.`);
-		client.user.setActivity(`with other packs.`, { type: "COMPETING" });
+		client.user.setActivity(`Tom Clancy's Rainbow Six: Siege`, { type: "COMPETING" });
 
 		client.config.discords.forEach((guild) => {
 			if (guild.channels.updateMember) client.updateMembers(guild.id, guild.channels.updateMember);
