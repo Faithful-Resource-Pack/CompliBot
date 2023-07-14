d297692bde6f28d0abd7f639f2e350aba24f32b7
Evorp <3vorpgaming@gmail.com>
2023-07-14 13:34:06 -0700

    cring module.exports usage

--- modified_libraries/GIFEncoder.js
+++ modified_libraries/GIFEncoder.js
@@ -6,10 +6,8 @@ const GIFEncoder = require("gif-encoder-2");
  * The original function can be found here:
  * https://github.com/benjaminadk/gif-encoder-2/blob/master/src/GIFEncoder.js#L241
  */
-class GIFEncoderFixed extends GIFEncoder {
+module.exports = class GIFEncoderFixed extends GIFEncoder {
 	findClosest(c) {
 		return 0;
 	}
 }
-
-module.exports = GIFEncoderFixed;
