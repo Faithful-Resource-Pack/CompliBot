f1acfbe2e3c92e7bd546576ef3746b8312befb65
Evorp <3vorpgaming@gmail.com>
2023-07-15 21:16:46 -0700

    Update ready.ts

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -5,7 +5,7 @@ export const event: Event = {
 	name: "ready",
 	run: async (client) => {
 		console.log(`${success}${client.user.username} is online!`);
-		client.user.setActivity(`for commands`, { type: "WATCHING" });
+		client.user.setActivity(`commands`, { type: "LISTENING" });
 
 		client.config.discords.forEach((guild) => {
 			if (guild.channels.updateMember) client.updateMembers(guild.id, guild.channels.updateMember);
