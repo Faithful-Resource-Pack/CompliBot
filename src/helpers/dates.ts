2189873f221fc794e65db18725568e088c610571
Evorp <3vorpgaming@gmail.com>
2023-07-18 14:53:36 -0700

    switch month and minute abbreviations

--- src/helpers/dates.ts
+++ src/helpers/dates.ts
@@ -30,13 +30,13 @@ export const addSeconds = (d: Date, seconds?: number): Date => {
 export const parseDate = (d: string): number => {
 	if ((d.endsWith("y") && !d.endsWith("day")) || d.endsWith("year") || d.endsWith("years"))
 		return parseInt(d, 10) * 24 * 3600 * 365;
-	if (d.endsWith("m") || d.endsWith("month") || d.endsWith("months"))
+	if (d.endsWith("mon") || d.endsWith("month") || d.endsWith("months"))
 		return parseInt(d, 10) * 30 * 24 * 3600;
 	if (d.endsWith("w") || d.endsWith("week") || d.endsWith("weeks"))
 		return parseInt(d, 10) * 7 * 86400;
 	if (d.endsWith("d") || d.endsWith("day") || d.endsWith("days")) return parseInt(d, 10) * 86400;
 	if (d.endsWith("h") || d.endsWith("hours") || d.endsWith("hour")) return parseInt(d, 10) * 3600;
-	if (d.endsWith("min") || d.endsWith("minute") || d.endsWith("minutes"))
+	if (d.endsWith("m") || d.endsWith("minute") || d.endsWith("minutes"))
 		return parseInt(d, 10) * 60;
 	if (d.endsWith("s") || d.endsWith("second") || d.endsWith("seconds")) return parseInt(d, 10);
 
