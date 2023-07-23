b36ed875dea9f71152bb005b30e12a13b8560733
Evorp <3vorpgaming@gmail.com>
2023-07-22 17:05:29 -0700

    Revert "remove unused channels and extras"
    This reverts commit 7f42645bacf9591ff562b5b251d19af485188f0b.

--- src/helpers/interfaces/config.ts
+++ src/helpers/interfaces/config.ts
@@ -8,6 +8,7 @@ export interface Discord {
 	team?: string; // tell if discord guilds are teamed up (for global commands)
 	name: string;
 	id: string;
+	channels: { [updateMember: string]: string };
 	roles?: { [role: string]: string };
 }
 
