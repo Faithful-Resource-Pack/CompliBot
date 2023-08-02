e903498d49bb2914ed7d478f09fb61432ac19e27
Evorp <3vorpgaming@gmail.com>
2023-08-02 13:37:11 -0700

    move comparison generation to its own function

---
+++ functions/submission/generateComparison.js
@@ -0,0 +1,73 @@
+const settings = require("../../resources/settings.json");
+const stitch = require("../textures/stitch");
+const { magnifyAttachment } = require("../textures/magnify");
+const { loadImage } = require("@napi-rs/canvas");
+
+/**
+ * @author Evorp
+ * @param {import("discord.js").Message} message
+ * @param {import("discord.js").MessageAttachment} attachment
+ * @param {{path: String, version: String, edition: String}} info
+ * @returns {Promise<{comparisonImage: MessageAttachment, hasReference: Boolean}>}
+ */
+module.exports = async function generateComparison(message, attachment, info) {
+	// determine reference image to compare against
+	let repoKey;
+	for (let [packKey, packValue] of Object.entries(settings.submission.packs)) {
+		if (packValue.channels.submit == message.channel.id) {
+			repoKey = packKey;
+			break;
+		}
+	}
+
+	let defaultRepo;
+	switch (repoKey) {
+		case "faithful_64x":
+			defaultRepo = settings.repositories.raw.faithful_32x;
+			break;
+		case "classic_faithful_64x":
+			defaultRepo = settings.repositories.raw.classic_faithful_32x;
+			break;
+		case "classic_faithful_32x_progart":
+			defaultRepo = settings.repositories.raw.progart;
+			break;
+		default:
+			defaultRepo = settings.repositories.raw.default;
+			break;
+	}
+
+	const upscaledImage = await loadImage(attachment.url);
+	let defaultImage;
+
+	// load images necessary to generate comparison
+	try {
+		defaultImage = await loadImage(
+			`${defaultRepo[info.edition.toLowerCase()]}${info.version}/${info.path}`,
+		);
+	} catch {
+		// reference texture doesn't exist so we use the default repo
+		defaultImage = await loadImage(
+			`${settings.repositories.raw.default[info.edition.toLowerCase()]}${info.version}/${
+				info.path
+			}`,
+		);
+	}
+
+	/** @type {import("@napi-rs/canvas").Image[]} */
+	let images = [defaultImage, upscaledImage];
+
+	try {
+		const currentImage = await loadImage(
+			`${settings.repositories.raw[repoKey][info.edition.toLowerCase()]}${info.version}/${
+				info.path
+			}`,
+		);
+		images.push(currentImage);
+	} catch {
+		// texture being submitted is a new texture, so there's nothing to compare against
+	}
+
+	// actually stitch the generated images
+	const stitched = await stitch(images);
+	return { comparisonImage: await magnifyAttachment(stitched, "compared.png"), hasReference: images.length == 3 };
+}
--- functions/submission/makeEmbed.js
+++ functions/submission/makeEmbed.js
@@ -2,14 +2,13 @@ const settings = require("../../resources/settings.json");
 const DEBUG = process.env.DEBUG.toLowerCase() == "true";
 
 const minecraftSorter = require("../../helpers/minecraftSorter");
-const getImages = require("../../helpers/getImages");
-const { imageButtons, submissionButtons } = require("../../helpers/buttons");
+
 const getDimensions = require("../textures/getDimensions");
-const stitch = require("../textures/stitch");
-const { magnifyBuffer, magnifyAttachment } = require("../textures/magnify");
+const getImages = require("../../helpers/getImages");
+const generateComparison = require("./generateComparison");
+const { imageButtons, submissionButtons } = require("../../helpers/buttons");
 
 const { MessageEmbed, MessageAttachment } = require("discord.js");
-const { loadImage } = require("@napi-rs/canvas");
 
 /**
  * Make a submission embed using existing texture information
@@ -32,6 +31,7 @@ module.exports = async function makeEmbed(
 	let pathText = [];
 	let imgButtons;
 
+	// generate displayed paths and uses
 	for (let use of uses) {
 		const localPath = await use.paths();
 		pathText.push(`**${use.editions[0].charAt(0).toUpperCase() + use.editions[0].slice(1)}**\n`);
@@ -69,74 +69,10 @@ module.exports = async function makeEmbed(
 	const rawImage = new MessageAttachment(attachment.url, `${texture.name}.png`);
 	const dimension = await getDimensions(attachment.url);
 
-	/**
-	 * COMPARISON IMAGE GENERATOR
-	 * should probably be its own function but oh well
-	 */
+	// generate comparison image if possible
 	if (dimension.width * dimension.height <= 262144) {
 		if (DEBUG) console.log(`Generating comparison image for texture: ${texture.name}`);
-
-		// determine reference image to compare against
-		let repoKey;
-		for (let [packKey, packValue] of Object.entries(settings.submission.packs)) {
-			if (packValue.channels.submit == message.channel.id) {
-				repoKey = packKey;
-				break;
-			}
-		}
-
-		let defaultRepo;
-		switch (repoKey) {
-			case "faithful_64x":
-				defaultRepo = settings.repositories.raw.faithful_32x;
-				break;
-			case "classic_faithful_64x":
-				defaultRepo = settings.repositories.raw.classic_faithful_32x;
-				break;
-			case "classic_faithful_32x_progart":
-				defaultRepo = settings.repositories.raw.progart;
-				break;
-			default:
-				defaultRepo = settings.repositories.raw.default;
-				break;
-		}
-		const upscaledImage = await loadImage(attachment.url);
-		let defaultImage;
-
-		// load images necessary to generate comparison
-		try {
-			defaultImage = await loadImage(
-				`${defaultRepo[info.edition.toLowerCase()]}${info.version}/${info.path}`,
-			);
-		} catch {
-			// reference texture doesn't exist so we use the default repo
-			defaultImage = await loadImage(
-				`${settings.repositories.raw.default[info.edition.toLowerCase()]}${info.version}/${
-					info.path
-				}`,
-			);
-		}
-
-		/** @type {import("@napi-rs/canvas").Image[] */
-		let images = [defaultImage, upscaledImage];
-
-		try {
-			const currentImage = await loadImage(
-				`${settings.repositories.raw[repoKey][info.edition.toLowerCase()]}${info.version}/${
-					info.path
-				}`,
-			);
-			images.push(currentImage);
-			imgButtons = [submissionButtons];
-		} catch {
-			// texture being submitted is a new texture, so there's nothing to compare against
-			imgButtons = [imageButtons];
-		}
-
-		const stitched = await stitch(images);
-
-		// generate comparison and add to embed
-		const comparisonImage = await magnifyAttachment(stitched, "compared.png");
+		const { comparisonImage, hasReference } = await generateComparison(message, attachment, info);
 		const [thumbnailUrl, comparedUrl] = await getImages(client, rawImage, comparisonImage);
 
 		embed.setImage(comparedUrl);
@@ -144,9 +80,14 @@ module.exports = async function makeEmbed(
 
 		// if the texture doesn't exist yet only include the default/new caption rather than everything
 		embed.setFooter({
-			text: images.length == 3 ? "Reference | New | Current" : "Reference | New",
+			text: hasReference ? "Reference | New | Current" : "Reference | New",
 		});
+
+		imgButtons = hasReference ? [submissionButtons] : [imageButtons];
+
 	} else {
+		if (DEBUG) console.log(`Texture is too big to generate comparison, loading directly instead: ${texture.name}`);
+
 		// image is too big so we just add it directly to the embed without comparison
 		const [imageUrl] = await getImages(client, attachment);
 		embed
