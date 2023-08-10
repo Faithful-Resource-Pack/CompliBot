6b9219e4b0ddaaee9e1ae6120fb4857716575095
Evorp <75297863+3vorp@users.noreply.github.com>
2023-08-10 16:04:42 -0700

    Update formattedDate.js

--- helpers/formattedDate.js
+++ helpers/formattedDate.js
@@ -6,7 +6,7 @@
 module.exports = function formattedDate(format = "dmy") {
 	const today = new Date();
 	// adding one for days and months since it starts at zero
-	const dd = String(today.getDate() - 1).padStart(2, "0");
+	const dd = String(today.getDate()).padStart(2, "0");
 	const mm = String(today.getMonth() + 1).padStart(2, "0");
 	const yyyy = today.getFullYear();
 
