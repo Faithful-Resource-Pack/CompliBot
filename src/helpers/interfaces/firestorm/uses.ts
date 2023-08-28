9768e27e78f26b47266a3068923b2108cc709650
Evorp <3vorpgaming@gmail.com>
2023-08-27 18:33:37 -0700

    forgor

--- src/helpers/interfaces/firestorm/uses.ts
+++ src/helpers/interfaces/firestorm/uses.ts
@@ -2,6 +2,5 @@ export interface Use {
 	id: string;
 	name: string;
 	edition: "java" | "bedrock" | "dungeons";
-	assets: string | null; // assets folder name (default: "minecraft") (null if bedrock/dungeons edition)
 }
 export interface Uses extends Array<Use> {}
