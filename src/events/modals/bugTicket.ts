dbebc7d50869d64666433f49049abb490b03a07b
Evorp <3vorpgaming@gmail.com>
2023-09-30 14:01:42 -0700

    implement submission bot's feedback system

--- src/events/modals/bugTicket.ts
+++ src/events/modals/bugTicket.ts
@@ -1,10 +1,33 @@
 import { Component } from "@interfaces";
 import { info } from "@helpers/logger";
-import { Client, ModalSubmitInteraction } from "@client";
+import { Client, EmbedBuilder, ModalSubmitInteraction } from "@client";
+import sendFeedback from "@helpers/feedback";
 
 export default {
 	id: "bugTicket",
 	async execute(client: Client, interaction: ModalSubmitInteraction) {
 		if (client.verbose) console.log(`${info}Bug report submitted!`);
+		await interaction.reply({
+			embeds: [
+				new EmbedBuilder()
+					.setTitle("Bug report received!")
+					.setDescription(
+						"Your ticket will be created shortly on GitHub. Thank you for helping us improve the bot!",
+					),
+			],
+		});
+
+		// I know there's a lot of copy/paste but this is the best djs can do
+		const title = `[Bug] ${interaction.fields.getTextInputValue("bugTitle")}`;
+		const whatHappened = interaction.fields.getTextInputValue("bugWhatHappened");
+		const toReproduce = interaction.fields.getTextInputValue("bugToReproduce");
+		const notes = interaction.fields.getTextInputValue("bugNotes");
+
+		const description =
+			`### What happened?\n\n${whatHappened}\n\n` +
+			`### To reproduce\n\n${toReproduce}\n\n` +
+			`### Notes\n\n${notes || "*No response*"}`;
+
+		return sendFeedback(interaction, title, description);
 	},
 } as Component;
--- src/events/modals/suggestionTicket.ts
+++ src/events/modals/suggestionTicket.ts
@@ -1,10 +1,32 @@
 import { Component } from "@interfaces";
 import { info } from "@helpers/logger";
-import { Client, ModalSubmitInteraction } from "@client";
+import { Client, EmbedBuilder, ModalSubmitInteraction } from "@client";
+import sendFeedback from "@helpers/feedback";
 
 export default {
 	id: "suggestionTicket",
 	async execute(client: Client, interaction: ModalSubmitInteraction) {
 		if (client.verbose) console.log(`${info}Suggestion submitted!`);
+		await interaction.reply({
+			embeds: [
+				new EmbedBuilder()
+					.setTitle("Feature request received!")
+					.setDescription(
+						"Your ticket will be created shortly on GitHub. Thank you for helping us improve the bot!",
+					),
+			],
+		});
+
+		const title = `[Feature] ${interaction.fields.getTextInputValue("suggestionTitle")}`;
+		const isProblem = interaction.fields.getTextInputValue("suggestionIsProblem");
+		const describe = interaction.fields.getTextInputValue("suggestionDescription");
+		const notes = interaction.fields.getTextInputValue("suggestionNotes");
+
+		const description =
+			`### Is your feature request related to a problem?\n\n${isProblem || "*No response*"}\n\n` +
+			`### Describe the feature you'd like\n\n${describe}\n\n` +
+			`### Notes\n\n${notes || "*No response*"}\n\n`;
+
+		return sendFeedback(interaction, title, description);
 	},
 } as Component;
