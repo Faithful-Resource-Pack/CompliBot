f12838eee8bc91c93c79fb6b3192bc355a296663
Evorp <3vorpgaming@gmail.com>
2023-11-20 12:59:25 -0800

    make api token optional (not used currently)

--- src/interfaces/tokens.ts
+++ src/interfaces/tokens.ts
@@ -2,10 +2,10 @@ export interface Tokens {
 	prefix: string; // for shorthand prefix commands
 	token: string;
 	apiUrl: string;
-	apiPassword: string; // for api authentification
+	apiPassword?: string; // for api authentication
 	errorChannel: string; // error channel where errors are sent
-	developers: string[];
-	gitToken: string;
+	developers: string[]; // people authorized to use dev-only commands and features
+	gitToken: string; // for feedback system
 	dev: boolean; // if true: instantiate command only it the bot dev discord server
 	verbose: boolean; // show extra logs for debugging
 	maintenance: boolean; // if true: disallow normal bot functions and update status
