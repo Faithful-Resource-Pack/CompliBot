9ae8e2f3c008c7dfa6b838c211a95ebafbc76ba5
Evorp <3vorpgaming@gmail.com>
2023-12-03 16:29:07 -0800

    rename colors to ColorManager to match repo naming scheme

--- src/helpers/images/palette.ts
+++ src/helpers/images/palette.ts
@@ -1,7 +1,7 @@
 import { EmbedBuilder } from "@client";
 import { ImageData, createCanvas, loadImage } from "@napi-rs/canvas";
 import { AttachmentBuilder } from "discord.js";
-import ColorManager from "@images/colors";
+import ColorManager from "@images/ColorManager";
 import { ImageSource } from "@images/getImage";
 
 const COOLORS_URL = "https://coolors.co/";
