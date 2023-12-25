a6656876934c01cfb8eae5095142d6985b8beb4b
Evorp <3vorpgaming@gmail.com>
2023-12-24 23:43:33 -0800

    coin permission

---
+++ src/components/buttons/general/reflip.ts
@@ -0,0 +1,52 @@
+import { Component } from "@interfaces/components";
+import { Client, ButtonInteraction, EmbedBuilder } from "@client";
+import { colors } from "@utility/colors";
+import { info } from "@helpers/logger";
+
+export default {
+	id: "reflip",
+	async execute(client: Client, interaction: ButtonInteraction) {
+		if (client.verbose) console.log(`${info}Coin re-flipped!`);
+
+		const messageInteraction = interaction.message.interaction;
+		const message = interaction.message;
+
+		if (messageInteraction != undefined && interaction.user.id != messageInteraction.user.id)
+			return interaction.reply({
+				embeds: [
+					new EmbedBuilder()
+						.setTitle(interaction.strings().error.permission.notice)
+						.setDescription(
+							interaction
+								.strings()
+								.error.permission.user_locked.replace(
+									"%USER%",
+									`<@!${messageInteraction.user.id}>`,
+								),
+						)
+						.setColor(colors.red),
+				],
+				ephemeral: true,
+			});
+
+		const res = Math.round(Math.random() * 100) / 100; // round to 2 decimal places
+
+		const embed = EmbedBuilder.from(message.embeds[0])
+			.setTitle(res > 0.5 ? "Heads" : res < 0.5 ? "Tails" : "Edge?!")
+			.setThumbnail(
+				`https://database.faithfulpack.net/images/bot/${
+					res > 0.5 ? "coin_heads" : res < 0.5 ? "coin_tails" : "coin_edge"
+				}.png?w=240&enlarge=1`,
+			);
+
+		await message.edit({ embeds: [embed] });
+		return interaction.reply({
+			embeds: [
+				new EmbedBuilder()
+					.setTitle(interaction.strings().command.coin.reflip.title)
+					.setDescription(interaction.strings().command.coin.reflip.description),
+			],
+			ephemeral: true,
+		});
+	},
+} as Component;
