be9ee3e8b422684e53815d7f548550591069027d
Evorp <3vorpgaming@gmail.com>
2025-03-12 23:58:30 -0700

    update cf urls

--- src/helpers/utility/infoembed.ts
+++ src/helpers/utility/infoembed.ts
@@ -45,19 +45,19 @@ export const media: Record<string, { title: string, description: string }> = {
 	classic_faithful_32x_progart: {
 		title: "Classic Faithful 32x:",
 		description: `
-[${parseID(emojis.main_logo)} Website](https://faithfulpack.net/classicfaithful/32x-programmer-art)
+[${parseID(emojis.main_logo)} Website](https://faithfulpack.net/classic32x)
 
-[${parseID(emojis.curseforge)} CurseForge](https://curseforge.com/minecraft/texture-packs/classic-faithful-32x-pa)
+[${parseID(emojis.curseforge)} CurseForge](https://www.curseforge.com/minecraft/texture-packs/classic-faithful-32x32)
 
 [${parseID(emojis.pmc)} Planet Minecraft](https://planetminecraft.com/texture-pack/classic-faithful-32x-pa)
 
-[${parseID(emojis.modrinth)} Modrinth](https://modrinth.com/resourcepack/classic-faithful-32x-programmer-art)
+[${parseID(emojis.modrinth)} Modrinth](https://modrinth.com/resourcepack/classic-faithful-32x)
 
-[${parseID(emojis.github)} Java Edition GitHub](https://github.com/classicfaithful/32x-programmer-art)
+[${parseID(emojis.github)} Java Edition GitHub](https://github.com/classicfaithful/classic-32x-java)
 
-[${parseID(emojis.github)} Bedrock Edition GitHub](https://github.com/classicfaithful/32x-programmer-art-bedrock)
+[${parseID(emojis.github)} Bedrock Edition GitHub](https://github.com/classicfaithful/classic-32x-bedrock)
 
-[${parseID(emojis.github)} Add-ons GitHub](https://github.com/classicfaithful/32x-programmer-art-add-ons)
+[${parseID(emojis.github)} Add-ons GitHub](https://github.com/classicfaithful/classic-32x-add-ons)
     `,
 	},
 
@@ -65,34 +65,34 @@ export const media: Record<string, { title: string, description: string }> = {
 	classic_faithful_32x: {
 		title: "Classic Faithful 32x Jappa:",
 		description: `
-[${parseID(emojis.main_logo)} Website](https://faithfulpack.net/classicfaithful/32x-jappa)
+[${parseID(emojis.main_logo)} Website](https://faithfulpack.net/classic32x-jappa)
 
-[${parseID(emojis.curseforge)} CurseForge](https://curseforge.com/minecraft/texture-packs/classic-faithful-32x-jappa)
+[${parseID(emojis.curseforge)} CurseForge](https://www.curseforge.com/minecraft/texture-packs/classic-faithful-32x-jappa)
 
 [${parseID(emojis.modrinth)} Modrinth](https://modrinth.com/resourcepack/classic-faithful-32x-jappa)
 
 [${parseID(emojis.pmc)} Planet Minecraft](https://planetminecraft.com/texture-pack/classic-faithful-32x)
 
-[${parseID(emojis.github)} Java Edition GitHub](https://github.com/classicfaithful/32x-jappa)
+[${parseID(emojis.github)} Java Edition GitHub](https://github.com/classicfaithful/classic-32x-jappa-java)
 
-[${parseID(emojis.github)} Bedrock Edition GitHub](https://github.com/classicfaithful/32x-jappa-bedrock)
+[${parseID(emojis.github)} Bedrock Edition GitHub](https://github.com/classicfaithful/classic-32x-jappa-bedrock)
 
-[${parseID(emojis.github)} Add-ons GitHub](https://github.com/classicfaithful/32x-jappa-add-ons)
+[${parseID(emojis.github)} Add-ons GitHub](https://github.com/classicfaithful/classic-32x-jappa-add-ons)
     `,
 	},
 
 	classic_faithful_64x: {
 		title: "Classic Faithful 64x Jappa:",
 		description: `
-[${parseID(emojis.main_logo)} Website](https://faithfulpack.net/classicfaithful/64x-jappa)
+[${parseID(emojis.main_logo)} Website](https://faithfulpack.net/classic64x-jappa)
 
-[${parseID(emojis.curseforge)} CurseForge](https://curseforge.com/minecraft/texture-packs/classic-faithful-64x)
+[${parseID(emojis.curseforge)} CurseForge](https://www.curseforge.com/minecraft/texture-packs/classic-faithful-64x-jappa)
 
 [${parseID(emojis.pmc)} Planet Minecraft](https://planetminecraft.com/texture-pack/classic-faithful-64x/)
 
-[${parseID(emojis.github)} Java Edition GitHub](https://github.com/classicfaithful/64x-jappa)
+[${parseID(emojis.github)} Java Edition GitHub](https://github.com/classicfaithful/classic-64x-jappa-java)
 
-[${parseID(emojis.github)} Bedrock Edition GitHub](https://github.com/classicfaithful/64x-jappa-bedrock)
+[${parseID(emojis.github)} Bedrock Edition GitHub](https://github.com/classicfaithful/classic-64x-jappa-bedrock)
     `,
 	},
 
@@ -102,7 +102,7 @@ export const media: Record<string, { title: string, description: string }> = {
 ### General:
 [Website](https://faithfulpack.net/) • [Docs](https://docs.faithfulpack.net/) • [Web App](https://webapp.faithfulpack.net) • [License](https://faithfulpack.net/license) • [Translate](https://translate.faithfulpack.net/)
 ### Listings:
-[Website](https://faithfulpack.net/news) • [CurseForge](https://curseforge.com/members/faithful_resource_pack/projects) • [Modrinth](https://modrinth.com/user/faithful-resource-pack) • [Planet Minecraft](https://planetminecraft.com/member/faithful_resource_pack/) • [MCPEDL](https://mcpedl.com/user/faithful-resource-pack/)
+[Website](https://faithfulpack.net/news) • [CurseForge](https://www.curseforge.com/members/faithful_resource_pack/projects) • [Modrinth](https://modrinth.com/user/faithful-resource-pack) • [Planet Minecraft](https://planetminecraft.com/member/faithful_resource_pack/) • [MCPEDL](https://mcpedl.com/user/faithful-resource-pack/)
 ### Media:
 [Twitter](https://twitter.com/faithfulpack/) • [Bluesky](https://bsky.app/profile/faithfulpack.net) • [Reddit](https://reddit.com/r/faithfulpack/) • [Main GitHub](https://github.com/faithful-resource-pack/) • [Classic Faithful GitHub](https://github.com/classicfaithful/)`,
 	},
