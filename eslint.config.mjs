cab67eb7d4ce362986c562124221e8b009560845
Evorp <75297863+3vorp@users.noreply.github.com>
2026-01-06 12:33:31 -1000

    chore(lint): update eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,8 @@
+import { defineConfig } from "eslint/config";
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended, {
+export default defineConfig(eslint.configs.recommended, tseslint.configs.recommended, {
 	rules: {
 		"@typescript-eslint/no-require-imports": "off",
 		// pretty much all any uses are needed because discord.js sucks
